b90ba23d7df2b56a75a58e6e345231a48d9d4599
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2021-03-05 13:08:42 -0500

    spicedb: main with zap/cobra/metrics/signals

--- cmd/spicedb/main.go
+++ cmd/spicedb/main.go
@@ -1,60 +1,140 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
+	"context"
+	"errors"
 	"net"
+	"net/http"
+	"net/http/pprof"
+	"os"
+	"os/signal"
 
+	"github.com/jzelinskie/cobrautil"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
 
 	api "github.com/authzed/spicedb/internal/REDACTEDapi/api"
 	health "github.com/authzed/spicedb/internal/REDACTEDapi/healthcheck"
+	"github.com/authzed/spicedb/internal/datastore"
 	"github.com/authzed/spicedb/internal/datastore/memdb"
 	"github.com/authzed/spicedb/internal/services"
 )
 
-var (
-	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	certFile = flag.String("cert_file", "", "The TLS cert file")
-	keyFile  = flag.String("key_file", "", "The TLS key file")
-	port     = flag.Int("port", 10000, "The server port")
-)
-
 func main() {
-	flag.Parse()
-	endpoint := fmt.Sprintf("localhost:%d", *port)
-	lis, err := net.Listen("tcp", endpoint)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	var rootCmd = &cobra.Command{
+		Use:               "spicedb",
+		Short:             "A tuple store for ACLs.",
+		PersistentPreRunE: cobrautil.SyncViperPreRunE("CALADAN"),
+		Run:               rootRun,
 	}
-	var opts []grpc.ServerOption
-	if *tls {
-		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+
+	rootCmd.Flags().String("grpc-addr", ":50051", "address to listen on for serving gRPC services")
+	rootCmd.Flags().String("grpc-cert-path", "", "local path to the TLS certificate used to serve gRPC services")
+	rootCmd.Flags().String("grpc-key-path", "", "local path to the TLS key used to serve gRPC services")
+	rootCmd.Flags().Bool("grpc-no-tls", false, "serve unencrypted gRPC services")
+	rootCmd.Flags().String("metrics-addr", ":9090", "address to listen on for serving metrics and profiles")
+	rootCmd.Flags().Bool("log-debug", false, "enable logging debug events")
+
+	rootCmd.Execute()
+}
+
+func rootRun(cmd *cobra.Command, args []string) {
+	logger, _ := zap.NewProduction()
+	if cobrautil.MustGetBool(cmd, "log-debug") {
+		logger, _ = zap.NewDevelopment()
+	}
+	defer logger.Sync()
+
+	var grpcServer *grpc.Server
+	if cobrautil.MustGetBool(cmd, "grpc-no-tls") {
+		grpcServer = grpc.NewServer()
+	} else {
+		var err error
+		grpcServer, err = NewTlsGrpcServer(
+			cobrautil.MustGetStringExpanded(cmd, "grpc-cert-path"),
+			cobrautil.MustGetStringExpanded(cmd, "grpc-key-path"),
+		)
 		if err != nil {
-			log.Fatalf("Failed to generate credentials %v", err)
+			logger.Fatal("failed to create TLS gRPC server", zap.Error(err))
 		}
-		opts = []grpc.ServerOption{grpc.Creds(creds)}
 	}
-	grpcServer := grpc.NewServer(opts...)
 
 	nsDatastore, err := memdb.NewMemdbNamespaceDatastore()
 	if err != nil {
-		log.Fatalf("Failed to instantiate datastore: %v", err)
+		logger.Fatal("failed to init in-memory namespace datastore", zap.Error(err))
 	}
 
-	api.RegisterACLServiceServer(grpcServer, services.NewACLServer())
-	api.RegisterNamespaceServiceServer(grpcServer, services.NewNamespaceServer(nsDatastore))
-	api.RegisterWatchServiceServer(grpcServer, services.NewWatchServer())
-	health.RegisterHealthServer(grpcServer, services.NewHealthServer())
-	reflection.Register(grpcServer)
+	RegisterGrpcServices(grpcServer, nsDatastore)
 
-	log.Printf("Listening on endpoint: %s", endpoint)
-	err = grpcServer.Serve(lis)
+	go func() {
+		addr := cobrautil.MustGetString(cmd, "grpc-addr")
+		l, err := net.Listen("tcp", addr)
+		if err != nil {
+			logger.Fatal("failed to listen on addr for gRPC server", zap.Error(err), zap.String("addr", addr))
+		}
+
+		logger.Info("gRPC server started listening", zap.String("addr", addr))
+		grpcServer.Serve(l)
+	}()
+
+	metricsrv := NewMetricsServer(cobrautil.MustGetString(cmd, "metrics-addr"))
+	go func() {
+		if err := metricsrv.ListenAndServe(); err != http.ErrServerClosed {
+			logger.Fatal("failed while serving metrics", zap.Error(err))
+		}
+	}()
+
+	signalctx, _ := signal.NotifyContext(context.Background(), os.Interrupt)
+	for {
+		select {
+		case <-signalctx.Done():
+			logger.Info("received interrupt")
+			grpcServer.GracefulStop()
+
+			if err := metricsrv.Close(); err != nil {
+				logger.Fatal("failed while shutting down metrics server", zap.Error(err))
+			}
+			return
+		}
+	}
+}
+
+func NewMetricsServer(addr string) *http.Server {
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+	return &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+}
+
+func RegisterGrpcServices(srv *grpc.Server, nsds datastore.NamespaceDatastore) {
+	api.RegisterACLServiceServer(srv, services.NewACLServer())
+	api.RegisterNamespaceServiceServer(srv, services.NewNamespaceServer(nsds))
+	api.RegisterWatchServiceServer(srv, services.NewWatchServer())
+	health.RegisterHealthServer(srv, services.NewHealthServer())
+	reflection.Register(srv)
+}
+
+func NewTlsGrpcServer(certPath, keyPath string) (*grpc.Server, error) {
+	if certPath != "" && keyPath != "" {
+		return nil, errors.New("missing one of required values: cert path, key path")
+	}
+
+	creds, err := credentials.NewServerTLSFromFile(certPath, keyPath)
 	if err != nil {
-		log.Printf("Error running server: %v", err)
+		return nil, err
 	}
+
+	return grpc.NewServer(grpc.Creds(creds)), nil
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,17 @@ module github.com/authzed/spicedb
 go 1.16
 
 require (
-	github.com/golang/protobuf v1.4.2
+	github.com/golang/protobuf v1.4.3
 	github.com/hashicorp/go-memdb v1.3.1
+	github.com/jzelinskie/cobrautil v0.0.0-20210305182230-ea9666fe73f4
+	github.com/prometheus/client_golang v1.9.0
+	github.com/spf13/cobra v1.1.3
 	github.com/stretchr/testify v1.5.1
+	go.uber.org/zap v1.16.0
 	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
 	golang.org/x/sys v0.0.0-20210112080510-489259a85091 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto v0.0.0-20200806141610-86f49bd18e98 // indirect
 	google.golang.org/grpc v1.36.0
 	google.golang.org/protobuf v1.25.0
-	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
