fed435bdb76346047189c3ff4bcfc4247876b60e
Joseph Schorr <josephschorr@users.noreply.github.com>
2021-05-04 12:20:17 -0400

    Wrapper server in zed-testserver which multiplexes to different SpiceDB services based on the incoming token

--- cmd/zed-testserver/main.go
+++ cmd/zed-testserver/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/jzelinskie/cobrautil"
@@ -17,6 +18,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
+	grpcauth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"github.com/authzed/spicedb/internal/datastore"
 	"github.com/authzed/spicedb/internal/datastore/memdb"
 	"github.com/authzed/spicedb/internal/graph"
@@ -57,28 +59,14 @@ func main() {
 func runTestServer(cmd *cobra.Command, args []string) {
 	grpcServer := grpc.NewServer()
 
-	ds, err := memdb.NewMemdbDatastore(0, REVISION_FUZZING_DURATION, GC_WINDOW, 0)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to init datastore")
+	configFilePaths := cobrautil.MustGetStringSlice(cmd, "load-configs")
+	server := &tokenBasedServer{
+		configFilePaths: configFilePaths,
 	}
 
-	// Populate the datastore for any configuration files specified.
-	err = validationfile.PopulateFromFiles(ds, cobrautil.MustGetStringSlice(cmd, "load-configs"))
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to load config files")
-	}
-
-	nsm, err := namespace.NewCachingNamespaceManager(ds, NS_CACHE_EXPIRATION, nil)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to initialize namespace manager")
-	}
-
-	dispatch, err := graph.NewLocalDispatcher(nsm, ds)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to initialize check dispatcher")
-	}
-
-	RegisterGrpcServices(grpcServer, ds, nsm, dispatch, MAX_DEPTH)
+	api.RegisterACLServiceServer(grpcServer, server)
+	api.RegisterNamespaceServiceServer(grpcServer, server)
+	reflection.Register(grpcServer)
 
 	go func() {
 		addr := cobrautil.MustGetString(cmd, "grpc-addr")
@@ -100,16 +88,94 @@ func runTestServer(cmd *cobra.Command, args []string) {
 	}
 }
 
-func RegisterGrpcServices(
-	srv *grpc.Server,
-	ds datastore.Datastore,
-	nsm namespace.Manager,
-	dispatch graph.Dispatcher,
-	maxDepth uint16,
-) {
-	api.RegisterACLServiceServer(srv, services.NewACLServer(ds, nsm, dispatch, maxDepth))
-	api.RegisterNamespaceServiceServer(srv, services.NewNamespaceServer(ds))
-	reflection.Register(srv)
+type model struct {
+	datastore        datastore.Datastore
+	namespaceManager namespace.Manager
+	dispatcher       graph.Dispatcher
+}
+
+type tokenBasedServer struct {
+	api.UnimplementedACLServiceServer
+	api.UnimplementedNamespaceServiceServer
+
+	configFilePaths []string
+	modelByToken    sync.Map
+}
+
+func (tbs *tokenBasedServer) modelForContext(ctx context.Context) model {
+	tokenStr, _ := grpcauth.AuthFromMD(ctx, "bearer")
+	cached, hasModel := tbs.modelByToken.Load(tokenStr)
+	if hasModel {
+		return cached.(model)
+	}
+
+	log.Info().Str("token", tokenStr).Msg("initializing new model for token")
+	model := tbs.createModel()
+	tbs.modelByToken.Store(tokenStr, model)
+	return model
+}
+
+func (tbs *tokenBasedServer) nsServer(ctx context.Context) api.NamespaceServiceServer {
+	model := tbs.modelForContext(ctx)
+	return services.NewNamespaceServer(model.datastore)
+}
+
+func (tbs *tokenBasedServer) WriteConfig(ctx context.Context, req *api.WriteConfigRequest) (*api.WriteConfigResponse, error) {
+	return tbs.nsServer(ctx).WriteConfig(ctx, req)
+}
+
+func (tbs *tokenBasedServer) ReadConfig(ctx context.Context, req *api.ReadConfigRequest) (*api.ReadConfigResponse, error) {
+	return tbs.nsServer(ctx).ReadConfig(ctx, req)
+}
+
+func (tbs *tokenBasedServer) aclServer(ctx context.Context) api.ACLServiceServer {
+	model := tbs.modelForContext(ctx)
+	return services.NewACLServer(model.datastore, model.namespaceManager, model.dispatcher, MAX_DEPTH)
+}
+
+func (tbs *tokenBasedServer) Read(ctx context.Context, req *api.ReadRequest) (*api.ReadResponse, error) {
+	return tbs.aclServer(ctx).Read(ctx, req)
+}
+
+func (tbs *tokenBasedServer) Write(ctx context.Context, req *api.WriteRequest) (*api.WriteResponse, error) {
+	return tbs.aclServer(ctx).Write(ctx, req)
+}
+
+func (tbs *tokenBasedServer) Check(ctx context.Context, req *api.CheckRequest) (*api.CheckResponse, error) {
+	return tbs.aclServer(ctx).Check(ctx, req)
+}
+
+func (tbs *tokenBasedServer) ContentChangeCheck(ctx context.Context, req *api.ContentChangeCheckRequest) (*api.CheckResponse, error) {
+	return tbs.aclServer(ctx).ContentChangeCheck(ctx, req)
+}
+
+func (tbs *tokenBasedServer) Expand(ctx context.Context, req *api.ExpandRequest) (*api.ExpandResponse, error) {
+	return tbs.aclServer(ctx).Expand(ctx, req)
+}
+
+func (tbs *tokenBasedServer) createModel() model {
+	ds, err := memdb.NewMemdbDatastore(0, REVISION_FUZZING_DURATION, GC_WINDOW, 0)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to init datastore")
+	}
+
+	// Populate the datastore for any configuration files specified.
+	err = validationfile.PopulateFromFiles(ds, tbs.configFilePaths)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to load config files")
+	}
+
+	nsm, err := namespace.NewCachingNamespaceManager(ds, NS_CACHE_EXPIRATION, nil)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to initialize namespace manager")
+	}
+
+	dispatch, err := graph.NewLocalDispatcher(nsm, ds)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to initialize check dispatcher")
+	}
+
+	return model{ds, nsm, dispatch}
 }
 
 func persistentPreRunE(cmd *cobra.Command, args []string) error {
