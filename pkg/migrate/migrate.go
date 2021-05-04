c4f659d3601d6c8a45f06d39c9dc5d06ffcb95aa
Jake Moshenko <jacob.moshenko@gmail.com>
2021-05-04 18:25:28 -0400

    spicedb: Add a pure go migration framework.

---
+++ pkg/migrate/migrate.go
@@ -0,0 +1,170 @@
+package migrate
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+const (
+	Head = "head"
+	None = ""
+)
+
+// Driver represents the common interface for reading and writing the revision
+// data from a migrateable backing datastore.
+type Driver interface {
+	// Version returns the current version of the schema in the backing datastore.
+	// If the datastore is brand new, version should return the empty string without
+	// an error.
+	Version() (string, error)
+
+	// WriteVersion records the newly migrated version to the backing datastore.
+	WriteVersion(version, replaced string) error
+}
+
+type migration struct {
+	version  string
+	replaces string
+	up       interface{}
+}
+
+var migrations map[string]migration = make(map[string]migration)
+
+// Register is used to associate a single migration with the migration engine.
+// The up parameter should be a function that performs the actual upgrade logic
+// and which takes a pointer to a concrete implementation of the Driver
+// interface as its only parameters, which will be passed direcly from the Run
+// method into the upgrade function. If not extra fields or data are required
+// the function can alternatively take a Driver interface param.
+func Register(version, replaces string, up interface{}) error {
+	if _, ok := migrations[version]; ok {
+		return fmt.Errorf("revision already exists: %s", version)
+	}
+
+	migrations[version] = migration{
+		version:  version,
+		replaces: replaces,
+		up:       up,
+	}
+
+	return nil
+}
+
+// Run will actually perform the necessary migrations to bring the backing datastore
+// from its current revision to the specified revision.
+func Run(driver Driver, throughRevision string) error {
+	starting, err := driver.Version()
+	if err != nil {
+		return fmt.Errorf("unable to compute target revision: %w", err)
+	}
+
+	if throughRevision == Head {
+		throughRevision, err = computeHeadRevision(migrations)
+		if err != nil {
+			return fmt.Errorf("unable to compute head revision: %w", err)
+		}
+	}
+
+	toRun, err := collectMigrationsInRange(starting, throughRevision, migrations)
+	if err != nil {
+		return fmt.Errorf("unable to compute migration list: %w", err)
+	}
+
+	for _, oneMigration := range toRun {
+		// Check that the migration is actually a function that accepts one parameter, the type of
+		// which is a valid coercion of driver instance.
+		if err := checkTypes(driver, oneMigration.up); err != nil {
+			return fmt.Errorf("unable to validate up migration: %w", err)
+		}
+	}
+
+	for _, migrationToRun := range toRun {
+		// Double check that the current version reported is the one we expect
+		currentVersion, err := driver.Version()
+		if err != nil {
+			return fmt.Errorf("unable to load version from driver: %w", err)
+		}
+
+		if migrationToRun.replaces != currentVersion {
+			return fmt.Errorf("migration attempting to run out of order: %s != %s", currentVersion, migrationToRun.replaces)
+		}
+
+		in := []reflect.Value{reflect.ValueOf(driver)}
+		upFunction := reflect.ValueOf(migrationToRun.up)
+
+		errArg := upFunction.Call(in)[0]
+		if !errArg.IsNil() {
+			return fmt.Errorf("error running migration up function: %v", errArg)
+		}
+
+		if err := driver.WriteVersion(migrationToRun.version, migrationToRun.replaces); err != nil {
+			return fmt.Errorf("error writing migration version to driver: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func computeHeadRevision(allMigrations map[string]migration) (string, error) {
+	candidates := make(map[string]struct{}, len(allMigrations))
+	for candidate := range allMigrations {
+		candidates[candidate] = struct{}{}
+	}
+
+	for _, eliminateReplaces := range allMigrations {
+		delete(candidates, eliminateReplaces.replaces)
+	}
+
+	var allHeads []string
+	for headRevision := range candidates {
+		allHeads = append(allHeads, headRevision)
+	}
+
+	if len(allHeads) != 1 {
+		return "", fmt.Errorf("multiple or zero head revisions found: %v", allHeads)
+	}
+
+	return allHeads[0], nil
+}
+
+func collectMigrationsInRange(starting, through string, all map[string]migration) ([]migration, error) {
+	var found []migration
+
+	lookingForRevision := through
+	for lookingForRevision != starting {
+		foundMigration, ok := all[lookingForRevision]
+		if !ok {
+			return []migration{}, fmt.Errorf("unable to find migration for revision: %s", lookingForRevision)
+		}
+
+		found = append([]migration{foundMigration}, found...)
+		lookingForRevision = foundMigration.replaces
+	}
+
+	return found, nil
+}
+
+func checkTypes(driver Driver, shouldBeFunction interface{}) error {
+	driverType := reflect.TypeOf(driver)
+	if driverType == nil {
+		return errors.New("driver is nil pointer")
+	}
+
+	funcType := reflect.TypeOf(shouldBeFunction)
+	if funcType == nil {
+		return errors.New("function pointer is nil")
+	}
+
+	if funcType.NumIn() != 1 {
+		return errors.New("function must take one and only one parameter")
+	}
+
+	onlyParam := funcType.In(0)
+
+	if !driverType.AssignableTo(onlyParam) {
+		return errors.New("driver object must be assignable to function parameter")
+	}
+
+	return nil
+}
---
+++ pkg/migrate/migrate_test.go
@@ -0,0 +1,152 @@
+package migrate
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type fakeDriver struct {
+}
+
+func (*fakeDriver) Version() (string, error) {
+	return "", nil
+}
+
+func (*fakeDriver) WriteVersion(version, replaced string) error {
+	return nil
+}
+
+func TestParameterChecking(t *testing.T) {
+	testCases := []struct {
+		driver      Driver
+		up          interface{}
+		expectError bool
+	}{
+		{&fakeDriver{}, func(d Driver) {}, false},
+		{&fakeDriver{}, func(fd *fakeDriver) {}, false},
+		{nil, nil, true},
+		{&fakeDriver{}, nil, true},
+		{&fakeDriver{}, func(a *int) {}, true},
+		{nil, func(a *int) {}, true},
+		{nil, &fakeDriver{}, true},
+	}
+
+	require := require.New(t)
+	for _, tc := range testCases {
+		err := checkTypes(tc.driver, tc.up)
+		require.Equal(tc.expectError, err != nil, err)
+	}
+}
+
+type revisionRangeTest struct {
+	start, end        string
+	expectError       bool
+	expectedRevisions []string
+}
+
+func TestRevisionWalking(t *testing.T) {
+	testCases := []struct {
+		migrations map[string]migration
+		ranges     []revisionRangeTest
+	}{
+		{noMigrations, []revisionRangeTest{
+			{"", "", false, []string{}},
+			{"", "123", true, []string{}},
+		}},
+		{simpleMigrations, []revisionRangeTest{
+			{"", "123", false, []string{"123"}},
+			{"123", "123", false, []string{}},
+			{"", "", false, []string{}},
+			{"", "456", true, []string{}},
+			{"123", "456", true, []string{}},
+			{"456", "456", false, []string{}},
+		}},
+		{singleHeadedChain, []revisionRangeTest{
+			{"", "123", false, []string{"123"}},
+			{"", "789", false, []string{"123", "456", "789"}},
+			{"123", "789", false, []string{"456", "789"}},
+			{"123", "456", false, []string{"456"}},
+			{"123", "10", true, []string{}},
+			{"", "10", true, []string{}},
+		}},
+		{multiHeadedChain, []revisionRangeTest{
+			{"", "123", false, []string{"123"}},
+			{"", "789a", false, []string{"123", "456", "789a"}},
+			{"", "789b", false, []string{"123", "456", "789b"}},
+			{"456", "789b", false, []string{"789b"}},
+		}},
+		{missingEarlyMigrations, []revisionRangeTest{
+			{"", "123", true, []string{}},
+			{"", "10", true, []string{}},
+			{"123", "10", false, []string{"456", "789", "10"}},
+			{"456", "10", false, []string{"789", "10"}},
+		}},
+	}
+
+	require := require.New(t)
+
+	for _, tc := range testCases {
+		for _, versionRange := range tc.ranges {
+			computed, err := collectMigrationsInRange(
+				versionRange.start,
+				versionRange.end,
+				tc.migrations,
+			)
+
+			require.Equal(versionRange.expectError, err != nil, err)
+
+			migrationNames := make([]string, 0, len(computed))
+			for _, mgr := range computed {
+				migrationNames = append(migrationNames, mgr.version)
+			}
+			require.Equal(versionRange.expectedRevisions, migrationNames)
+		}
+	}
+}
+
+func TestComputeHeadRevision(t *testing.T) {
+	testCases := []struct {
+		migrations   map[string]migration
+		headRevision string
+		expectError  bool
+	}{
+		{noMigrations, "", true},
+		{simpleMigrations, "123", false},
+		{singleHeadedChain, "789", false},
+		{multiHeadedChain, "", true},
+		{missingEarlyMigrations, "10", false},
+	}
+
+	require := require.New(t)
+	for _, tc := range testCases {
+		head, err := computeHeadRevision(tc.migrations)
+		require.Equal(tc.expectError, err != nil, err)
+		require.Equal(tc.headRevision, head)
+	}
+}
+
+var noMigrations = map[string]migration{}
+
+var simpleMigrations = map[string]migration{
+	"123": {"123", "", nil},
+}
+
+var singleHeadedChain = map[string]migration{
+	"123": {"123", "", nil},
+	"456": {"456", "123", nil},
+	"789": {"789", "456", nil},
+}
+
+var multiHeadedChain = map[string]migration{
+	"123":  {"123", "", nil},
+	"456":  {"456", "123", nil},
+	"789a": {"789a", "456", nil},
+	"789b": {"789b", "456", nil},
+}
+
+var missingEarlyMigrations = map[string]migration{
+	"456": {"456", "123", nil},
+	"789": {"789", "456", nil},
+	"10":  {"10", "789", nil},
+}
