a18dd5578ae2ce035494d7c36bcfe16a36963328
Sam Kim <samkim@gmail.com>
2021-06-15 14:45:19 -0700

    Add datastore attr to tracing span

--- internal/datastore/postgres/log_tracer.go
+++ internal/datastore/postgres/log_tracer.go
@@ -13,5 +13,5 @@ type tracingLogger struct {
 
 func (tl tracingLogger) Log(ctx context.Context, level pgx.LogLevel, msg string, data map[string]interface{}) {
 	span := trace.SpanFromContext(ctx)
-	span.AddEvent(msg, trace.WithAttributes(attribute.Stringer("level", level)))
+	span.AddEvent(msg, trace.WithAttributes(attribute.Stringer("level", level), attribute.String("datastore", "postgres")))
 }
