a36bae6e7572eeafdf0cb6903c9a5d9518c0652c
Joseph Schorr <josephschorr@users.noreply.github.com>
2021-09-20 13:55:55 -0400

    Move root run to a `serve` subcommand
    Signed-off-by: Joseph Schorr <josephschorr@users.noreply.github.com>

--- cmd/spicedb/main.go
+++ cmd/spicedb/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/fatih/color"
 	"github.com/jzelinskie/cobrautil"
 )
 
@@ -15,28 +11,8 @@ var persistentPreRunE = cobrautil.CommandStack(
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		d := color.New(color.FgHiWhite, color.Bold)
-		d.Println("SpiceDB")
-		fmt.Println("-----------------------------------------------")
-
-		fmt.Println()
-		color.White("Example Usage (%s):", color.YellowString("No TLS and in-memory"))
-		fmt.Println()
-		fmt.Println("\tspicedb --grpc-preshared-key \"somerandomkeyhere\" --grpc-no-tls")
-		fmt.Println()
-
-		fmt.Println()
-		color.White("Example Usage (%s):", color.GreenString("With TLS and a real datastore"))
-		fmt.Println()
-		fmt.Println("\tspicedb --grpc-preshared-key \"realkeyhere\" --grpc-cert-path path/to/tls/cert")
-		fmt.Println("\t        --grpc-key-path path/to/tls/key --datastore-engine postgres")
-		fmt.Println("\t        --datastore-conn-uri \"postgres-connection-string-here\"")
-
-		return
-	}
-
 	rootCmd := newRootCmd()
+	registerServeCmd(rootCmd)
 	registerMigrateCmd(rootCmd)
 	registerHeadCmd(rootCmd)
 	registerDeveloperServiceCmd(rootCmd)
