6290327ecf8e01227414afa26dabf175557703bb
Sean Bryant <sbryant@github.com>
2022-01-20 19:03:23 +0000

    goimport fmt

--- internal/datastore/mysql/mysql.go
+++ internal/datastore/mysql/mysql.go
@@ -6,6 +6,7 @@ import (
 
 	v0 "github.com/authzed/authzed-go/proto/authzed/api/v0"
 	v1 "github.com/authzed/authzed-go/proto/authzed/api/v1"
+
 	"github.com/authzed/spicedb/internal/datastore"
 	"github.com/authzed/spicedb/internal/datastore/options"
 )
