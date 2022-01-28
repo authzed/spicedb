be026a5d9b3ea91e21133878f1a4fbaaec8d4137
Sean Bryant <sbryant@github.com>
2022-01-28 22:23:30 +0000

    Refactor mysql test
    Only test the migration driver for mysql.
    
    Remove unused functions and fields. These will be added as the datastore
    is implemented .
    
    The migration test asserts the latest migration has run successfully.

--- internal/datastore/mysql/mysql_test.go
+++ internal/datastore/mysql/mysql_test.go
@@ -8,32 +8,23 @@ import (
 	"fmt"
 	"log"
 	"testing"
-	"time"
 
-	"github.com/alecthomas/units"
 	"github.com/ory/dockertest/v3"
 	"github.com/stretchr/testify/require"
 
-	"github.com/authzed/spicedb/internal/datastore"
-	"github.com/authzed/spicedb/internal/datastore/common"
 	"github.com/authzed/spicedb/internal/datastore/mysql/migrations"
 	"github.com/authzed/spicedb/pkg/migrate"
 	"github.com/authzed/spicedb/pkg/secrets"
 )
 
 var (
-	testDBName = "spicedb_test"
-	creds      = "root:secret"
+	testDBPrefix = "spicedb_test"
+	creds        = "root:secret"
 )
 
 type sqlTest struct {
-	db                        *sql.DB
-	port                      string
-	dbName                    string
-	creds                     string
-	connectStr                string
-	splitAtEstimatedQuerySize units.Base2Bytes
-	cleanup                   func()
+	connectStr string
+	cleanup    func()
 }
 
 var mysqlContainer = &dockertest.RunOptions{
@@ -42,45 +33,13 @@ var mysqlContainer = &dockertest.RunOptions{
 	Env:        []string{"MYSQL_ROOT_PASSWORD=secret"},
 }
 
-func (st sqlTest) New(revisionFuzzingTimedelta, gcWindow time.Duration, watchBufferLength uint16) (datastore.Datastore, error) {
-	_, err := createDbAndRunMigrations(st.db, st.dbName, st.connectStr)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create database and run migrations: %w", err)
-	}
-
-	return NewMysqlDatastore(
-		st.connectStr,
-		// RevisionFuzzingTimedelta(revisionFuzzingTimedelta),
-		// GCWindow(gcWindow),
-		// GCInterval(0*time.Second), // Disable auto GC
-		// WatchBufferLength(watchBufferLength),
-		// SplitAtEstimatedQuerySize(st.splitAtEstimatedQuerySize),
-	)
-}
-
-func createDbAndRunMigrations(db *sql.DB, dbName string, connectStr string) (string, error) {
-	_, err := db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", dbName))
-	if err != nil {
-		return "", fmt.Errorf("unable to create database: %w", err)
-	}
-
+func createMigrationDriver(connectStr string) (*migrations.MysqlDriver, error) {
 	migrationDriver, err := migrations.NewMysqlDriver(connectStr)
 	if err != nil {
-		return "", fmt.Errorf("unable to initialize migration engine: %w", err)
+		return nil, fmt.Errorf("unable to initialize migration engine: %w", err)
 	}
 
-	version, err := migrationDriver.Version()
-	if err != nil {
-		return "", fmt.Errorf("unable to get starting migration version: %w", err)
-	}
-	if version == "" {
-		err = migrations.Manager.Run(migrationDriver, migrate.Head, migrate.LiveRun)
-		if err != nil {
-			return "", fmt.Errorf("unable to migrate database: %w", err)
-		}
-	}
-
-	return migrationDriver.Version()
+	return migrationDriver, nil
 }
 
 func TestMySQLMigrations(t *testing.T) {
@@ -89,32 +48,41 @@ func TestMySQLMigrations(t *testing.T) {
 	sqlTester := newTester(mysqlContainer, creds, 3306)
 	defer sqlTester.cleanup()
 
-	version, err := createDbAndRunMigrations(sqlTester.db, sqlTester.dbName, sqlTester.connectStr)
+	migrationDriver, err := createMigrationDriver(sqlTester.connectStr)
 	req.NoError(err)
-	req.Equal("initial", version)
+
+	version, err := migrationDriver.Version()
+	req.NoError(err)
+	req.Equal("", version)
+
+	err = migrations.Manager.Run(migrationDriver, migrate.Head, migrate.LiveRun)
+	req.NoError(err)
+
+	version, err = migrationDriver.Version()
+	req.NoError(err)
+
+	headVersion, err := migrations.Manager.HeadRevision()
+	req.NoError(err)
+	req.Equal(headVersion, version)
 }
-
-// func TestMySqlDatastore(t *testing.T) {
-// 	sqlTester := newTester(mysqlContainer, creds, 3306)
-// 	defer sqlTester.cleanup()
-
-// 	test.All(t, sqlTester)
-// }
-
-// func TestMySqlDatastoreWithSplit(t *testing.T) {
-// 	// Set the split at a VERY small size, to ensure any WithUsersets queries are split.
-// 	sqlTester := newTester(mysqlContainer, creds, 3306)
-// 	sqlTester.splitAtEstimatedQuerySize = 1 // bytes
-// 	defer sqlTester.cleanup()
-
-// 	test.All(t, sqlTester)
-// }
 
 func newTester(containerOpts *dockertest.RunOptions, creds string, portNum uint16) *sqlTest {
 	pool, err := dockertest.NewPool("")
 	if err != nil {
 		log.Fatalf("Could not connect to docker: %s", err)
 	}
+
+	uniquePortion, err := secrets.TokenHex(4)
+	if err != nil {
+		log.Fatalf("Could not generate unique portion of db name: %s", err)
+	}
+	dbName := testDBPrefix + uniquePortion
+
+	// Setup the containter environment. From the mysql docker image docs:
+	// MYSQL_DATABASE
+	// This variable is optional and allows you to specify the name of a database to be created on image startup.
+	// If a user/password was supplied (see below) then that user will be granted superuser access (corresponding to GRANT ALL) to this database.
+	containerOpts.Env = append(containerOpts.Env, fmt.Sprintf("MYSQL_DATABASE=%s", dbName))
 
 	resource, err := pool.RunWithOptions(containerOpts)
 	if err != nil {
@@ -123,7 +91,8 @@ func newTester(containerOpts *dockertest.RunOptions, creds string, portNum uint1
 
 	var db *sql.DB
 	port := resource.GetPort(fmt.Sprintf("%d/tcp", portNum))
-	db, err = sql.Open("mysql", fmt.Sprintf("%s@(localhost:%s)/mysql", creds, port))
+	connectStr := fmt.Sprintf("%s@(localhost:%s)/%s", creds, port, dbName)
+	db, err = sql.Open("mysql", connectStr)
 	if err != nil {
 		log.Fatalf("couldn't open DB: %s", err)
 	}
@@ -139,14 +108,6 @@ func newTester(containerOpts *dockertest.RunOptions, creds string, portNum uint1
 		log.Fatalf("mysql database error: %v", err)
 	}
 
-	uniquePortion, err := secrets.TokenHex(4)
-	if err != nil {
-		log.Fatalf("Could not generate unique portion of db name: %s", err)
-	}
-
-	dbName := testDBName + uniquePortion
-	connectStr := fmt.Sprintf("%s@(localhost:%s)/%s", creds, port, dbName)
-
 	cleanup := func() {
 		// When you're done, kill and remove the container
 		if err = pool.Purge(resource); err != nil {
@@ -155,12 +116,7 @@ func newTester(containerOpts *dockertest.RunOptions, creds string, portNum uint1
 	}
 
 	return &sqlTest{
-		db:                        db,
-		port:                      port,
-		dbName:                    dbName,
-		creds:                     creds,
-		connectStr:                connectStr,
-		cleanup:                   cleanup,
-		splitAtEstimatedQuerySize: common.DefaultSplitAtEstimatedQuerySize,
+		connectStr: connectStr,
+		cleanup:    cleanup,
 	}
 }
