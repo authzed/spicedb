156e7fe5b0013d4eb68fc612a75320fe6d49b26d
Chris Kirkland <chriskirkland@github.com>
2022-03-23 08:46:13 -0500

    add id column to mysql_migration_version table

--- internal/datastore/mysql/migrations/zz_migration.0001_initial_schema.go
+++ internal/datastore/mysql/migrations/zz_migration.0001_initial_schema.go
@@ -3,7 +3,13 @@ package migrations
 import "fmt"
 
 func createMysqlMigrationVersion(mysql *MysqlDriver) string {
-	return fmt.Sprintf("CREATE TABLE %s (_meta_version_ VARCHAR(255) NOT NULL PRIMARY KEY);",
+	// we need the additional primary key column because github.com/github/gh-ost requires a shared, not-null
+	// key between the _to_ and _from_ table schemas to perform a schema migration.
+	// -- https://github.com/github/gh-ost/blob/master/doc/shared-key.md
+	return fmt.Sprintf("CREATE TABLE %s "+
+		`( id int(11) NOT NULL PRIMARY KEY,
+		_meta_version_ VARCHAR(255) NOT NULL
+		);`,
 		mysql.mysqlMigrationVersionTable())
 }
 
