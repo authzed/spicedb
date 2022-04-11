f4239512890dd41a18378aa18ad198f1d613ed0c
Víctor Roldán Betancort <vroldanbet@github.com>
2022-04-11 16:56:09 +0200

    introduces mysql migration driver
    GitHub runs mysql schema migrations at scale using skeema
    (https://github.com/skeema/skeema) and gh-ost (https://github.com/github/gh-ost).
    The SpiceDB migration driver for MySQL is designed in a way it
    is compatible with migration workloads relying on those tools.
    
    The main difference is how the MySQL migration driver maintains
    the metadata needed to know which version is currently running. The
    PSQL migration driver follows a pattern similar to what's proposed
    by SQLAlchemy's Alembic, with a table that keeps track of the migration run.
    Because Skeema does not support data migrations, and seeding the database
    could be considered a data migration, we decided to encode the metadata
    into the database schema (DML) so we can expose the same information
    but only relying on schema migrations. As such the migration_version table
    is empty, and maintains the running version in a table column. This turns
    every migration into a pure DML operations, which Skeema understands well.
    
    The way we run these then is by executing zed migrate in development DB,
    and letting skeema detect the changes to the development database. These are
    then persisted, and deployed using our own internal migration automation
    buil on top of skeema and gh-ost.
    
    The other distiction with the PSQL migration driver is the support for table
    prefixes. This is necessary to align with our own internal conventions.
    
    Co-authored-by: Alex Slepak <ams11@github.com>
    Co-authored-by: Christopher Kirkland <chriskirkland@github.com>
    Co-authored-by: Hannah Gould <hagould@github.com>
    Co-authored-by: John Reed <johnpreed@github.com>
    Co-authored-by: Sean Bryant <sbryant@github.com>

---
+++ internal/datastore/mysql/migrations/driver.go
@@ -0,0 +1,124 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"strings"
+
+	sq "github.com/Masterminds/squirrel"
+	sqlDriver "github.com/go-sql-driver/mysql"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	errUnableToInstantiate       = "unable to instantiate MysqlDriver: %w"
+	mysqlMissingTableErrorNumber = 1146
+
+	migrationVersionColumnPrefix = "_meta_version_"
+)
+
+var sb = sq.StatementBuilder.PlaceholderFormat(sq.Question)
+
+type MysqlDriver struct {
+	db *sql.DB
+	*tables
+}
+
+// NewMysqlDriverFromDSN creates a new migration driver with a connection pool to the database DSN specified.
+//
+// URI: [scheme://][user[:[password]]@]host[:port][/schema][?attribute1=value1&attribute2=value2...
+// See // https://dev.mysql.com/doc/refman/8.0/en/connecting-using-uri-or-key-value-pairs.html
+func NewMysqlDriverFromDSN(url string, tablePrefix string) (*MysqlDriver, error) {
+	dbConfig, err := sqlDriver.ParseDSN(url)
+	if err != nil {
+		return nil, fmt.Errorf(errUnableToInstantiate, err)
+	}
+
+	db, err := sql.Open("mysql", dbConfig.FormatDSN())
+	if err != nil {
+		return nil, fmt.Errorf(errUnableToInstantiate, err)
+	}
+	err = sqlDriver.SetLogger(&log.Logger)
+	if err != nil {
+		return nil, fmt.Errorf("unable to set logging to mysql driver: %w", err)
+	}
+	return NewMysqlDriverFromDB(db, tablePrefix), nil
+}
+
+// NewMysqlDriverFromDB creates a new migration driver with a connection pool specified upfront.
+func NewMysqlDriverFromDB(db *sql.DB, tablePrefix string) *MysqlDriver {
+	return &MysqlDriver{db, newTables(tablePrefix)}
+}
+
+func revisionToColumnName(revision string) string {
+	return migrationVersionColumnPrefix + revision
+}
+
+func columnNameToRevision(columnName string) (string, bool) {
+	if !strings.HasPrefix(columnName, migrationVersionColumnPrefix) {
+		return "", false
+	}
+	return strings.TrimPrefix(columnName, migrationVersionColumnPrefix), true
+}
+
+// Version returns the version of the schema to which the connected database
+// has been migrated.
+func (driver *MysqlDriver) Version() (string, error) {
+	query, args, err := sb.Select("*").From(driver.migrationVersion()).ToSql()
+	if err != nil {
+		return "", fmt.Errorf("unable to load driver migration revision: %w", err)
+	}
+
+	rows, err := driver.db.Query(query, args...)
+	if err != nil {
+		var mysqlError *sqlDriver.MySQLError
+		if errors.As(err, &mysqlError) && mysqlError.Number == mysqlMissingTableErrorNumber {
+			return "", nil
+		}
+		return "", fmt.Errorf("unable to load driver migration revision: %w", err)
+	}
+	defer LogOnError(context.Background(), rows.Close)
+	if rows.Err() != nil {
+		return "", fmt.Errorf("unable to load driver migration revision: %w", err)
+	}
+	cols, err := rows.Columns()
+	if err != nil {
+		return "", fmt.Errorf("failed to get columns: %w", err)
+	}
+
+	for _, col := range cols {
+		if revision, ok := columnNameToRevision(col); ok {
+			return revision, nil
+		}
+	}
+	return "", errors.New("no migration version detected")
+}
+
+// WriteVersion overwrites the _meta_version_ column name which encodes the version
+// of the database schema.
+func (driver *MysqlDriver) WriteVersion(version, replaced string) error {
+	stmt := fmt.Sprintf("ALTER TABLE %s CHANGE %s %s VARCHAR(255) NOT NULL",
+		driver.migrationVersion(),
+		revisionToColumnName(replaced),
+		revisionToColumnName(version),
+	)
+	if _, err := driver.db.Exec(stmt); err != nil {
+		return fmt.Errorf("unable to version: %w", err)
+	}
+
+	return nil
+}
+
+func (driver *MysqlDriver) Close() error {
+	return driver.db.Close()
+}
+
+// LogOnError executes the function and logs the error.
+// Useful to avoid silently ignoring errors in defer statements
+func LogOnError(ctx context.Context, f func() error) {
+	if err := f(); err != nil {
+		log.Ctx(ctx).Error().Err(err)
+	}
+}
