1ecaa4a868e6f4c6180b677920d59e268df826ae
Víctor Roldán Betancort <vroldanbet@github.com>
2022-04-13 15:17:41 +0200

    pr review: do proper Sprintf and separate arguments with commas and newlines

--- internal/datastore/mysql/migrations/zz_migration.0001_initial_schema.go
+++ internal/datastore/mysql/migrations/zz_migration.0001_initial_schema.go
@@ -6,10 +6,11 @@ func createMysqlMigrationVersion(driver *MySQLDriver) string {
 	// we need the additional primary key column because github.com/github/gh-ost requires a shared, not-null
 	// key between the _to_ and _from_ table schemas to perform a schema migration.
 	// -- https://github.com/github/gh-ost/blob/master/doc/shared-key.md
-	return fmt.Sprintf("CREATE TABLE %s "+
-		`(id int(11) NOT NULL PRIMARY KEY,
+	return fmt.Sprintf(`CREATE TABLE %s (
+		id int(11) NOT NULL PRIMARY KEY,
 		_meta_version_ VARCHAR(255) NOT NULL);`,
-		driver.migrationVersion())
+		driver.migrationVersion(),
+	)
 }
 
 func init() {
--- internal/datastore/mysql/migrations/zz_migration.0003_indexes.go
+++ internal/datastore/mysql/migrations/zz_migration.0003_indexes.go
@@ -6,17 +6,20 @@ import (
 
 func createReverseQueryIndex(driver *MySQLDriver) string {
 	return fmt.Sprintf("CREATE INDEX ix_relation_tuple_by_subject ON %s (userset_object_id, userset_namespace, userset_relation, namespace, relation)",
-		driver.RelationTuple())
+		driver.RelationTuple(),
+	)
 }
 
 func createReverseCheckIndex(driver *MySQLDriver) string {
 	return fmt.Sprintf("CREATE INDEX ix_relation_tuple_by_subject_relation ON %s (userset_namespace, userset_relation, namespace, relation)",
-		driver.RelationTuple())
+		driver.RelationTuple(),
+	)
 }
 
 func createIndexOnTupleTransactionTimestamp(driver *MySQLDriver) string {
 	return fmt.Sprintf("CREATE INDEX ix_relation_tuple_transaction_by_timestamp on %s (timestamp)",
-		driver.RelationTupleTransaction())
+		driver.RelationTupleTransaction(),
+	)
 }
 
 func init() {
