3f11d50fa162787246a2dca7c25426830e11fc45
Jake Moshenko <jacob.moshenko@gmail.com>
2022-04-20 10:50:00 -0400

    mysql: use a stable unique ID for stats

--- internal/datastore/mysql/datastore.go
+++ internal/datastore/mysql/datastore.go
@@ -457,10 +457,16 @@ func (mds *Datastore) isSeeded(ctx context.Context) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	if headRevision != datastore.NoRevision {
-		return true, nil
+	if headRevision == datastore.NoRevision {
+		return false, nil
 	}
-	return false, nil
+
+	_, err = mds.getUniqueID(ctx)
+	if err != nil {
+		return false, nil
+	}
+
+	return true, nil
 }
 
 // seedDatabase initializes the first transaction revision if necessary.
@@ -492,15 +498,41 @@ func (mds *Datastore) seedDatabase(ctx context.Context) error {
 		return fmt.Errorf("seedDatabase: failed to get last inserted id: %w", err)
 	}
 
-	err = tx.Commit()
+	if lastInsertID != noLastInsertID {
+		// If there was no error and `lastInsertID` is 0, the insert was ignored. This indicates the transaction
+		// was already seeded by another processes (i.e. race condition).
+		log.Info().Int64("headRevision", lastInsertID).Msg("seeded base datastore headRevision")
+	}
+
+	uuidSQL, uuidArgs, err := sb.
+		Insert(mds.driver.Metadata()).
+		Options("IGNORE").
+		Columns(metadataIDColumn, metadataUniqueIDColumn).
+		Values(0, uuid.NewString()).
+		ToSql()
 	if err != nil {
-		return err
+		return fmt.Errorf("seedDatabase: failed to prepare SQL: %w", err)
+	}
+
+	insertUniqueResult, err := tx.ExecContext(ctx, uuidSQL, uuidArgs...)
+	if err != nil {
+		return fmt.Errorf("seedDatabase: failed to insert unique ID: %w", err)
+	}
+
+	lastInsertID, err = insertUniqueResult.LastInsertId()
+	if err != nil {
+		return fmt.Errorf("seedDatabase: failed to get last inserted unique id: %w", err)
 	}
 
 	if lastInsertID != noLastInsertID {
 		// If there was no error and `lastInsertID` is 0, the insert was ignored. This indicates the transaction
 		// was already seeded by another processes (i.e. race condition).
-		log.Info().Int64("headRevision", lastInsertID).Msg("seeded base datastore headRevision")
+		log.Info().Int64("headRevision", lastInsertID).Msg("seeded base datastore unique ID")
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
 	}
 
 	return nil
