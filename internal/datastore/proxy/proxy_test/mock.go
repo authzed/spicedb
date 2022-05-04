0f832c66adf068550b001d436b82c3ba9fda79ff
Jake Moshenko <jacob.moshenko@gmail.com>
2022-05-04 14:59:18 -0400

    datastore: v2 datastore proxy implementations

---
+++ internal/datastore/proxy/proxy_test/mock.go
@@ -0,0 +1,232 @@
+package proxy_test
+
+import (
+	"context"
+
+	v1 "github.com/authzed/authzed-go/proto/authzed/api/v1"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/authzed/spicedb/internal/datastore/options"
+	"github.com/authzed/spicedb/pkg/datastore"
+	core "github.com/authzed/spicedb/pkg/proto/core/v1"
+)
+
+type MockDatastore struct {
+	mock.Mock
+}
+
+func (dm *MockDatastore) SnapshotReader(rev datastore.Revision) datastore.Reader {
+	args := dm.Called(rev)
+	return args.Get(0).(datastore.Reader)
+}
+
+func (dm *MockDatastore) ReadWriteTx(
+	ctx context.Context,
+	f datastore.TxUserFunc,
+) (datastore.Revision, error) {
+	args := dm.Called()
+	mockRWT := args.Get(0).(datastore.ReadWriteTransaction)
+
+	if err := f(ctx, mockRWT); err != nil {
+		return datastore.NoRevision, err
+	}
+
+	return args.Get(1).(datastore.Revision), args.Error(2)
+}
+
+func (dm *MockDatastore) OptimizedRevision(ctx context.Context) (datastore.Revision, error) {
+	args := dm.Called()
+	return args.Get(0).(datastore.Revision), args.Error(1)
+}
+
+func (dm *MockDatastore) HeadRevision(ctx context.Context) (datastore.Revision, error) {
+	args := dm.Called()
+	return args.Get(0).(datastore.Revision), args.Error(1)
+}
+
+func (dm *MockDatastore) CheckRevision(ctx context.Context, revision datastore.Revision) error {
+	args := dm.Called(revision)
+	return args.Error(0)
+}
+
+func (dm *MockDatastore) Watch(ctx context.Context, afterRevision datastore.Revision) (<-chan *datastore.RevisionChanges, <-chan error) {
+	args := dm.Called(afterRevision)
+	return args.Get(0).(<-chan *datastore.RevisionChanges), args.Get(1).(<-chan error)
+}
+
+func (dm *MockDatastore) IsReady(ctx context.Context) (bool, error) {
+	args := dm.Called()
+	return args.Bool(0), args.Error(1)
+}
+
+func (dm *MockDatastore) Statistics(ctx context.Context) (datastore.Stats, error) {
+	args := dm.Called()
+	return args.Get(0).(datastore.Stats), args.Error(1)
+}
+
+func (dm *MockDatastore) Close() error {
+	args := dm.Called()
+	return args.Error(0)
+}
+
+type MockReader struct {
+	mock.Mock
+}
+
+func (dm *MockReader) ReadNamespace(
+	ctx context.Context,
+	nsName string,
+) (*core.NamespaceDefinition, datastore.Revision, error) {
+	args := dm.Called(nsName)
+
+	var def *core.NamespaceDefinition
+	if args.Get(0) != nil {
+		def = args.Get(0).(*core.NamespaceDefinition)
+	}
+
+	return def, args.Get(1).(datastore.Revision), args.Error(2)
+}
+
+func (dm *MockReader) QueryRelationships(
+	ctx context.Context,
+	filter *v1.RelationshipFilter,
+	options ...options.QueryOptionsOption,
+) (datastore.RelationshipIterator, error) {
+	callArgs := make([]interface{}, 0, len(options)+1)
+	callArgs = append(callArgs, filter)
+	for _, option := range options {
+		callArgs = append(callArgs, option)
+	}
+
+	args := dm.Called(callArgs...)
+	var results datastore.RelationshipIterator
+	if args.Get(0) != nil {
+		results = args.Get(0).(datastore.RelationshipIterator)
+	}
+
+	return results, args.Error(1)
+}
+
+func (dm *MockReader) ReverseQueryRelationships(
+	ctx context.Context,
+	subjectFilter *v1.SubjectFilter,
+	options ...options.ReverseQueryOptionsOption,
+) (datastore.RelationshipIterator, error) {
+	callArgs := make([]interface{}, 0, len(options)+1)
+	callArgs = append(callArgs, subjectFilter)
+	for _, option := range options {
+		callArgs = append(callArgs, option)
+	}
+
+	args := dm.Called(callArgs...)
+	var results datastore.RelationshipIterator
+	if args.Get(0) != nil {
+		results = args.Get(0).(datastore.RelationshipIterator)
+	}
+
+	return results, args.Error(1)
+}
+
+func (dm *MockReader) ListNamespaces(ctx context.Context) ([]*core.NamespaceDefinition, error) {
+	args := dm.Called()
+	return args.Get(0).([]*core.NamespaceDefinition), args.Error(1)
+}
+
+func (dm *MockReader) NamespaceCacheKey(namespaceName string) (string, error) {
+	args := dm.Called(namespaceName)
+	return args.String(0), args.Error(1)
+}
+
+type MockReadWriteTransaction struct {
+	mock.Mock
+}
+
+func (dm *MockReadWriteTransaction) ReadNamespace(
+	ctx context.Context,
+	nsName string,
+) (*core.NamespaceDefinition, datastore.Revision, error) {
+	args := dm.Called(nsName)
+
+	var def *core.NamespaceDefinition
+	if args.Get(0) != nil {
+		def = args.Get(0).(*core.NamespaceDefinition)
+	}
+
+	return def, args.Get(1).(datastore.Revision), args.Error(2)
+}
+
+func (dm *MockReadWriteTransaction) QueryRelationships(
+	ctx context.Context,
+	filter *v1.RelationshipFilter,
+	options ...options.QueryOptionsOption,
+) (datastore.RelationshipIterator, error) {
+	callArgs := make([]interface{}, 0, len(options)+1)
+	callArgs = append(callArgs, filter)
+	for _, option := range options {
+		callArgs = append(callArgs, option)
+	}
+
+	args := dm.Called(callArgs...)
+	var results datastore.RelationshipIterator
+	if args.Get(0) != nil {
+		results = args.Get(0).(datastore.RelationshipIterator)
+	}
+
+	return results, args.Error(1)
+}
+
+func (dm *MockReadWriteTransaction) ReverseQueryRelationships(
+	ctx context.Context,
+	subjectFilter *v1.SubjectFilter,
+	options ...options.ReverseQueryOptionsOption,
+) (datastore.RelationshipIterator, error) {
+	callArgs := make([]interface{}, 0, len(options)+1)
+	callArgs = append(callArgs, subjectFilter)
+	for _, option := range options {
+		callArgs = append(callArgs, option)
+	}
+
+	args := dm.Called(callArgs...)
+	var results datastore.RelationshipIterator
+	if args.Get(0) != nil {
+		results = args.Get(0).(datastore.RelationshipIterator)
+	}
+
+	return results, args.Error(1)
+}
+
+func (dm *MockReadWriteTransaction) ListNamespaces(ctx context.Context) ([]*core.NamespaceDefinition, error) {
+	args := dm.Called()
+	return args.Get(0).([]*core.NamespaceDefinition), args.Error(1)
+}
+
+func (dm *MockReadWriteTransaction) NamespaceCacheKey(namespaceName string) (string, error) {
+	args := dm.Called(namespaceName)
+	return args.String(0), args.Error(1)
+}
+
+func (dm *MockReadWriteTransaction) WriteRelationships(mutations []*v1.RelationshipUpdate) error {
+	args := dm.Called(mutations)
+	return args.Error(0)
+}
+
+func (dm *MockReadWriteTransaction) DeleteRelationships(filter *v1.RelationshipFilter) error {
+	args := dm.Called(filter)
+	return args.Error(0)
+}
+
+func (dm *MockReadWriteTransaction) WriteNamespaces(newConfigs ...*core.NamespaceDefinition) error {
+	args := dm.Called(newConfigs)
+	return args.Error(0)
+}
+
+func (dm *MockReadWriteTransaction) DeleteNamespace(nsName string) error {
+	args := dm.Called(nsName)
+	return args.Error(0)
+}
+
+var (
+	_ datastore.Datastore            = &MockDatastore{}
+	_ datastore.Reader               = &MockReader{}
+	_ datastore.ReadWriteTransaction = &MockReadWriteTransaction{}
+)
--- internal/datastore/proxy/readonly.go
+++ internal/datastore/proxy/readonly.go
@@ -3,12 +3,7 @@ package proxy
 import (
 	"context"
 
-	v1 "github.com/authzed/authzed-go/proto/authzed/api/v1"
-
-	core "github.com/authzed/spicedb/pkg/proto/core/v1"
-
-	"github.com/authzed/spicedb/internal/datastore"
-	"github.com/authzed/spicedb/internal/datastore/options"
+	"github.com/authzed/spicedb/pkg/datastore"
 )
 
 var errReadOnly = datastore.NewReadonlyErr()
@@ -23,8 +18,12 @@ func NewReadonlyDatastore(delegate datastore.Datastore) datastore.Datastore {
 	return roDatastore{delegate: delegate}
 }
 
-func (rd roDatastore) NamespaceCacheKey(namespaceName string, revision datastore.Revision) (string, error) {
-	return rd.delegate.NamespaceCacheKey(namespaceName, revision)
+func (rd roDatastore) SnapshotReader(rev datastore.Revision) datastore.Reader {
+	return rd.delegate.SnapshotReader(rev)
+}
+
+func (rd roDatastore) ReadWriteTx(context.Context, datastore.TxUserFunc) (datastore.Revision, error) {
+	return datastore.NoRevision, errReadOnly
 }
 
 func (rd roDatastore) Close() error {
@@ -35,14 +34,6 @@ func (rd roDatastore) IsReady(ctx context.Context) (bool, error) {
 	return rd.delegate.IsReady(ctx)
 }
 
-func (rd roDatastore) DeleteRelationships(ctx context.Context, _ []*v1.Precondition, _ *v1.RelationshipFilter) (datastore.Revision, error) {
-	return datastore.NoRevision, errReadOnly
-}
-
-func (rd roDatastore) WriteTuples(ctx context.Context, _ []*v1.Precondition, _ []*v1.RelationshipUpdate) (datastore.Revision, error) {
-	return datastore.NoRevision, errReadOnly
-}
-
 func (rd roDatastore) OptimizedRevision(ctx context.Context) (datastore.Revision, error) {
 	return rd.delegate.OptimizedRevision(ctx)
 }
@@ -51,46 +42,12 @@ func (rd roDatastore) HeadRevision(ctx context.Context) (datastore.Revision, err
 	return rd.delegate.HeadRevision(ctx)
 }
 
-func (rd roDatastore) Watch(ctx context.Context, afterRevision datastore.Revision) (<-chan *datastore.RevisionChanges, <-chan error) {
-	return rd.delegate.Watch(ctx, afterRevision)
-}
-
-func (rd roDatastore) WriteNamespace(ctx context.Context, newConfig *core.NamespaceDefinition) (datastore.Revision, error) {
-	return datastore.NoRevision, errReadOnly
-}
-
-func (rd roDatastore) ReadNamespace(ctx context.Context, nsName string, revision datastore.Revision) (*core.NamespaceDefinition, datastore.Revision, error) {
-	return rd.delegate.ReadNamespace(ctx, nsName, revision)
-}
-
-func (rd roDatastore) DeleteNamespace(ctx context.Context, nsName string) (datastore.Revision, error) {
-	return datastore.NoRevision, errReadOnly
-}
-
-func (rd roDatastore) QueryTuples(
-	ctx context.Context,
-	filter *v1.RelationshipFilter,
-	revision datastore.Revision,
-	options ...options.QueryOptionsOption,
-) (datastore.TupleIterator, error) {
-	return rd.delegate.QueryTuples(ctx, filter, revision, options...)
-}
-
-func (rd roDatastore) ReverseQueryTuples(
-	ctx context.Context,
-	subjectFilter *v1.SubjectFilter,
-	revision datastore.Revision,
-	options ...options.ReverseQueryOptionsOption,
-) (datastore.TupleIterator, error) {
-	return rd.delegate.ReverseQueryTuples(ctx, subjectFilter, revision, options...)
-}
-
 func (rd roDatastore) CheckRevision(ctx context.Context, revision datastore.Revision) error {
 	return rd.delegate.CheckRevision(ctx, revision)
 }
 
-func (rd roDatastore) ListNamespaces(ctx context.Context, revision datastore.Revision) ([]*core.NamespaceDefinition, error) {
-	return rd.delegate.ListNamespaces(ctx, revision)
+func (rd roDatastore) Watch(ctx context.Context, afterRevision datastore.Revision) (<-chan *datastore.RevisionChanges, <-chan error) {
+	return rd.delegate.Watch(ctx, afterRevision)
 }
 
 func (rd roDatastore) Statistics(ctx context.Context) (datastore.Stats, error) {
