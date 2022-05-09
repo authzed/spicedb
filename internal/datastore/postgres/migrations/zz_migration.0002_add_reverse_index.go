b00d1239b37285671cdaeee6f27fa18d3f278617
Jake Moshenko <jacob.moshenko@gmail.com>
2022-05-09 17:03:42 -0400

    postgres: convert postgres to datastore v2 API

--- internal/datastore/postgres/migrations/zz_migration.0002_add_reverse_index.go
+++ internal/datastore/postgres/migrations/zz_migration.0002_add_reverse_index.go
@@ -1,5 +1,11 @@
 package migrations
 
+import (
+	"context"
+
+	"github.com/jackc/pgx/v4"
+)
+
 const (
 	createReverseQueryIndex = `CREATE INDEX ix_relation_tuple_by_subject ON relation_tuple (userset_object_id, userset_namespace, userset_relation, namespace, relation)`
 	createReverseCheckIndex = `CREATE INDEX ix_relation_tuple_by_subject_relation ON relation_tuple (userset_namespace, userset_relation, namespace, relation)`
@@ -7,23 +13,20 @@ const (
 
 func init() {
 	if err := DatabaseMigrations.Register("add-reverse-index", "1eaeba4b8a73", func(apd *AlembicPostgresDriver) error {
-		tx, err := apd.db.Beginx()
-		if err != nil {
-			return err
-		}
-		defer tx.Rollback()
+		ctx := context.Background()
 
-		for _, stmt := range []string{
-			createReverseQueryIndex,
-			createReverseCheckIndex,
-		} {
-			_, err := tx.Exec(stmt)
-			if err != nil {
-				return err
+		return apd.db.BeginFunc(ctx, func(tx pgx.Tx) error {
+			for _, stmt := range []string{
+				createReverseQueryIndex,
+				createReverseCheckIndex,
+			} {
+				_, err := tx.Exec(ctx, stmt)
+				if err != nil {
+					return err
+				}
 			}
-		}
-
-		return tx.Commit()
+			return nil
+		})
 	}); err != nil {
 		panic("failed to register migration: " + err.Error())
 	}
--- internal/datastore/postgres/migrations/zz_migration.0005_change_transaction_timestamp_default.go
+++ internal/datastore/postgres/migrations/zz_migration.0005_change_transaction_timestamp_default.go
@@ -1,23 +1,17 @@
 package migrations
 
+import (
+	"context"
+)
+
 const alterTimestampDefaultValue = `
 	ALTER TABLE relation_tuple_transaction 
 		ALTER COLUMN timestamp SET DEFAULT (now() AT TIME ZONE 'UTC');`
 
 func init() {
 	if err := DatabaseMigrations.Register("change-transaction-timestamp-default", "add-transaction-timestamp-index", func(apd *AlembicPostgresDriver) error {
-		tx, err := apd.db.Beginx()
-		if err != nil {
-			return err
-		}
-		defer tx.Rollback()
-
-		_, err = tx.Exec(alterTimestampDefaultValue)
-		if err != nil {
-			return err
-		}
-
-		return tx.Commit()
+		_, err := apd.db.Exec(context.Background(), alterTimestampDefaultValue)
+		return err
 	}); err != nil {
 		panic("failed to register migration: " + err.Error())
 	}
