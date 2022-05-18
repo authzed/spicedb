6e32148bedd94c241d7719097e90dd5c7bd88f41
Evan Cordell <cordell.evan@gmail.com>
2022-05-18 17:15:30 -0400

    prefill

--- internal/datastore/crdb/migrations/zz_migration.0003_add_metadata_and_counters.go
+++ internal/datastore/crdb/migrations/zz_migration.0003_add_metadata_and_counters.go
@@ -3,6 +3,7 @@ package migrations
 import (
 	"context"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v4"
 )
@@ -14,7 +15,7 @@ const (
 
 	createCounters = `CREATE TABLE relationship_estimate_counters (
 	id BYTES PRIMARY KEY,
-	count INT NOT NULL
+	count INT NOT NULL DEFAULT 0
 );`
 
 	insertUniqueID = `INSERT INTO metadata (unique_id) VALUES ($1);`
@@ -33,6 +34,43 @@ func init() {
 			}
 
 			if _, err := tx.Exec(ctx, insertUniqueID, uuid.NewString()); err != nil {
+				return err
+			}
+
+			psql := sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
+			prefillCounterQuery := psql.Insert("relationship_estimate_counters").Columns(
+				"id",
+				"count",
+			)
+			for i := byte(0); i < 127; i++ {
+				for j := byte(0); j < 127; j++ {
+					prefillCounterQuery = prefillCounterQuery.Values([]byte{i, j}, 0)
+				}
+			}
+
+			sql, args, err := prefillCounterQuery.ToSql()
+			if err != nil {
+				return err
+			}
+			if _, err := tx.Exec(ctx, sql, args...); err != nil {
+				return err
+			}
+
+			prefillCounterQuery = psql.Insert("relationship_estimate_counters").Columns(
+				"id",
+				"count",
+			)
+			for i := byte(128); i < 255; i++ {
+				for j := byte(128); j < 255; j++ {
+					prefillCounterQuery = prefillCounterQuery.Values([]byte{i, j}, 0)
+				}
+			}
+
+			sql, args, err = prefillCounterQuery.ToSql()
+			if err != nil {
+				return err
+			}
+			if _, err := tx.Exec(ctx, sql, args...); err != nil {
 				return err
 			}
 
