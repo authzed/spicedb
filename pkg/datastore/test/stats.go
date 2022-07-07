2ec85f0e4bd058396a2aebd3dd9505923e7cc5d7
Joseph Schorr <josephschorr@users.noreply.github.com>
2022-07-07 13:39:36 -0400

    Add retries to MySQL stats test, which can occasionally get back empty stats

--- pkg/datastore/test/stats.go
+++ pkg/datastore/test/stats.go
@@ -3,11 +3,14 @@ package test
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/authzed/spicedb/internal/testfixtures"
 )
+
+const statsRetryCount = 3
 
 func StatsTest(t *testing.T, tester DatastoreTester) {
 	ctx := context.Background()
@@ -18,14 +21,23 @@ func StatsTest(t *testing.T, tester DatastoreTester) {
 
 	ds, _ = testfixtures.StandardDatastoreWithData(ds, require)
 
-	stats, err := ds.Statistics(ctx)
-	require.NoError(err)
+	for retryCount := statsRetryCount; retryCount >= 0; retryCount-- {
+		stats, err := ds.Statistics(ctx)
+		require.NoError(err)
 
-	require.Len(stats.UniqueID, 36, "unique ID must be a valid UUID")
-	require.Len(stats.ObjectTypeStatistics, 3, "must report object stats")
-	require.Greater(stats.EstimatedRelationshipCount, uint64(0), "must report some relationships")
+		require.Len(stats.UniqueID, 36, "unique ID must be a valid UUID")
+		require.Len(stats.ObjectTypeStatistics, 3, "must report object stats")
 
-	newStats, err := ds.Statistics(ctx)
-	require.NoError(err)
-	require.Equal(newStats.UniqueID, stats.UniqueID, "unique ID must be stable")
+		if stats.EstimatedRelationshipCount == uint64(0) && retryCount > 0 {
+			// Sleep for a bit to get the stats table to update.
+			time.Sleep(50 * time.Millisecond)
+			continue
+		}
+
+		require.Greater(stats.EstimatedRelationshipCount, uint64(0), "must report some relationships")
+
+		newStats, err := ds.Statistics(ctx)
+		require.NoError(err)
+		require.Equal(newStats.UniqueID, stats.UniqueID, "unique ID must be stable")
+	}
 }
