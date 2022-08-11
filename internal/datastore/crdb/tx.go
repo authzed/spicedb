2b1b3eb8bff00c8e590b221c1f7feb3ce69b88a5
Víctor Roldán Betancort <vroldanbet@authzed.com>
2022-08-11 16:11:05 +0100

    retry on 2 additional errors - unexpected EOF when connection is closed by CRDB - CRDB draining and not accepting new connections

--- internal/datastore/crdb/tx.go
+++ internal/datastore/crdb/tx.go
@@ -16,12 +16,14 @@ const (
 	crdbRetryErrCode = "40001"
 	// https://www.cockroachlabs.com/docs/stable/common-errors.html#result-is-ambiguous
 	crdbAmbiguousErrorCode = "40003"
+	// https://www.cockroachlabs.com/docs/stable/node-shutdown.html#connection-retry-loop
+	crdbServerNotAcceptingClients = "57P01"
 	// Error when SqlState is unknown
 	crdbUnknownSQLState = "XXUUU"
 	// Error message encountered when crdb nodes have large clock skew
 	crdbClockSkewMessage = "cannot specify timestamp in the future"
 
-	errReachedMaxAttempts = "maximum attempts reached (%d/%d): %w"
+	errReachedMaxRetries = "maximum retries reached (%d/%d): %w"
 )
 
 var resetHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
@@ -67,7 +69,7 @@ func executeWithResets(ctx context.Context, fn innerFunc, maxRetries uint8) (err
 	}
 
 	// The last error was resettable but we're out of retries
-	return fmt.Errorf(errReachedMaxAttempts, retries, maxRetries+1, err)
+	return fmt.Errorf(errReachedMaxRetries, retries, maxRetries+1, err)
 }
 
 func resettable(ctx context.Context, err error) bool {
@@ -78,12 +80,18 @@ func resettable(ctx context.Context, err error) bool {
 	if strings.Contains(err.Error(), "broken pipe") {
 		return true
 	}
+	// detect when cockroach closed a connection
+	if strings.Contains(err.Error(), "unexpected EOF") {
+		return true
+	}
 	sqlState := sqlErrorCode(ctx, err)
 	// Ambiguous result error includes connection closed errors
 	// https://www.cockroachlabs.com/docs/stable/common-errors.html#result-is-ambiguous
 	return sqlState == crdbAmbiguousErrorCode ||
 		// Reset for retriable errors
 		sqlState == crdbRetryErrCode ||
+		// Retry on node draining
+		sqlState == crdbServerNotAcceptingClients ||
 		// Error encountered when crdb nodes have large clock skew
 		(sqlState == crdbUnknownSQLState && strings.Contains(err.Error(), crdbClockSkewMessage))
 }
