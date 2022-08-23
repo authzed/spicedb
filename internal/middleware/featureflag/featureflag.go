0664f93fd54aefef7b063f0a406a26b915eaa247
Víctor Roldán Betancort <vroldanbet@authzed.com>
2022-08-23 19:38:28 +0100

    add comments and unexport flags type

--- internal/middleware/featureflag/featureflag.go
+++ internal/middleware/featureflag/featureflag.go
@@ -10,33 +10,39 @@ import (
 
 var key = struct{}{}
 
-type Flags map[string]struct{}
+type flags map[string]struct{}
 
+// WithFlag injects in the context the name of an enabled feature flag
 func WithFlag(ctx context.Context, flag string) context.Context {
-	flags := injector.FromContext[Flags](ctx, key)
-	if flags == nil {
-		flags = map[string]struct{}{flag: {}}
-		return injector.ContextWithValue(ctx, key, flags)
+	f := injector.FromContext[flags](ctx, key)
+	if f == nil {
+		f = map[string]struct{}{flag: {}}
+		return injector.ContextWithValue(ctx, key, f)
 	}
-	flags[flag] = struct{}{}
+	f[flag] = struct{}{}
 	return ctx
 }
 
+// FromContext returns a value of a feature flag specified by its name.
 func FromContext(ctx context.Context, flag string) bool {
-	flags := injector.FromContext[Flags](ctx, key)
-	_, ok := flags[flag]
+	f := injector.FromContext[flags](ctx, key)
+	_, ok := f[flag]
 	return ok
 }
 
+// UnaryServerInterceptor returns a new unary server interceptor that injects
+// the state of the applications feature flags in the context
 func UnaryServerInterceptor(flags []string) grpc.UnaryServerInterceptor {
-	return injector.UnaryServerInterceptor(key, FromStringSlice(flags))
+	return injector.UnaryServerInterceptor(key, fromStringSlice(flags))
 }
 
+// StreamServerInterceptor returns a new stream server interceptor that injects
+// the state of the applications feature flags in the context
 func StreamServerInterceptor(flags []string) grpc.StreamServerInterceptor {
-	return injector.StreamServerInterceptor(key, FromStringSlice(flags))
+	return injector.StreamServerInterceptor(key, fromStringSlice(flags))
 }
 
-func FromStringSlice(flags []string) Flags {
+func fromStringSlice(flags []string) flags {
 	flagMap := make(map[string]struct{}, len(flags))
 	for _, flag := range flags {
 		flagMap[flag] = struct{}{}
--- internal/middleware/injector/injector.go
+++ internal/middleware/injector/injector.go
@@ -9,10 +9,12 @@ import (
 
 type Key any
 
+// ContextWithValue injects a value in the context identified by the provided key
 func ContextWithValue[T any](ctx context.Context, key Key, value T) context.Context {
 	return context.WithValue(ctx, key, value)
 }
 
+// FromContext returns a value in the context specified by the provided key.
 func FromContext[T any](ctx context.Context, key Key) T {
 	var value T
 	if v := ctx.Value(key); v != nil {
@@ -21,12 +23,16 @@ func FromContext[T any](ctx context.Context, key Key) T {
 	return value
 }
 
+// UnaryServerInterceptor returns a new unary server interceptor that injects
+// state in the context identified by a specific key
 func UnaryServerInterceptor[T any](key Key, value T) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		return handler(ContextWithValue[T](ctx, key, value), req)
 	}
 }
 
+// StreamServerInterceptor returns a new stream server interceptor that injects
+// state in the context identified by a specific key
 func StreamServerInterceptor[T any](key Key, value T) grpc.StreamServerInterceptor {
 	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 		wrapped := middleware.WrapServerStream(stream)
