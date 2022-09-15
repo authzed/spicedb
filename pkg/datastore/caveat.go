0c9112f30fca3d3d11112c701ccd5e4d26778df1
Víctor Roldán Betancort <vroldanbet@authzed.com>
2022-09-15 19:37:05 +0100

    add interface docs

--- pkg/datastore/caveat.go
+++ pkg/datastore/caveat.go
@@ -6,15 +6,19 @@ import (
 	core "github.com/authzed/spicedb/pkg/proto/core/v1"
 )
 
+// ErrCaveatNotFound is the error returned when a caveat is not found by either ID or name
 var ErrCaveatNotFound = errors.New("caveat not found")
 
+// CaveatID serves as the system identifier for a persisted Caveat
 type CaveatID uint64
 
+// CaveatReader offers read operations for caveats
 type CaveatReader interface {
 	ReadCaveatByName(name string) (*core.Caveat, error)
 	ReadCaveatByID(ID CaveatID) (*core.Caveat, error)
 }
 
+// CaveatStorer offers both read and write operations for Caveats
 type CaveatStorer interface {
 	CaveatReader
 	WriteCaveats([]*core.Caveat) ([]CaveatID, error)
