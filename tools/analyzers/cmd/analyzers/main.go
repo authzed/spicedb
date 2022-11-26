dc3d085e85c3994c6553913b43b5a3590558622b
Joseph Schorr <josephschorr@users.noreply.github.com>
2022-11-25 20:33:23 -0500

    Add custom linter to find usages of specified types where Close() is not immediately synchronously called
    This will be used to find any iterators of tuples that have not been Close()-ed immediately, thus holding open database connections longer than necessary

--- tools/analyzers/cmd/analyzers/main.go
+++ tools/analyzers/cmd/analyzers/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"github.com/authzed/spicedb/tools/analyzers/closeafterusagecheck"
 	"github.com/authzed/spicedb/tools/analyzers/exprstatementcheck"
 	"github.com/authzed/spicedb/tools/analyzers/nilvaluecheck"
 	"golang.org/x/tools/go/analysis/multichecker"
 )
 
 func main() {
-	multichecker.Main(nilvaluecheck.Analyzer(), exprstatementcheck.Analyzer())
+	multichecker.Main(
+		nilvaluecheck.Analyzer(),
+		exprstatementcheck.Analyzer(),
+		closeafterusagecheck.Analyzer(),
+	)
 }
