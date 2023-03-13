154ad6304013e9e191228e9ed7640db64ccb4239
Víctor Roldán Betancort <vroldanbet@authzed.com>
2023-03-13 16:48:37 +0000

    adds revision GC test to all datastores
    makes sure that:
    - head revision is considered valid within GC Window
    - head revision is invalid outside GC Window
    - head-1 revision is not considered valid outside GC window

--- internal/datastore/spanner/spanner_test.go
+++ internal/datastore/spanner/spanner_test.go
@@ -16,9 +16,13 @@ import (
 
 func TestSpannerDatastore(t *testing.T) {
 	b := testdatastore.RunSpannerForTesting(t, "")
-	test.All(t, test.DatastoreTesterFunc(func(revisionQuantization, gcWindow time.Duration, watchBufferLength uint16) (datastore.Datastore, error) {
+	test.All(t, test.DatastoreTesterFunc(func(revisionQuantization, gcInterval, gcWindow time.Duration, watchBufferLength uint16) (datastore.Datastore, error) {
 		ds := b.NewDatastore(t, func(engine, uri string) datastore.Datastore {
-			ds, err := NewSpannerDatastore(uri, RevisionQuantization(revisionQuantization), GCWindow(gcWindow), WatchBufferLength(watchBufferLength))
+			ds, err := NewSpannerDatastore(uri,
+				RevisionQuantization(revisionQuantization),
+				GCWindow(gcWindow),
+				GCInterval(gcInterval),
+				WatchBufferLength(watchBufferLength))
 			require.NoError(t, err)
 			return ds
 		})
