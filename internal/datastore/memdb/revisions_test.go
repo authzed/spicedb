c3fde4f2b22e01b1e184b9d13e061631fce230e6
Víctor Roldán Betancort <vroldanbet@authzed.com>
2023-03-13 12:05:47 +0000

    fixes failures with FullyConsistent when GC window elapses
    When the latest known datastore revision is older than the
    GC Window, memdb would return consistently "invalid zedtoken:
    revision has expired".
    
    The proposed fix is to consider the last revision as valid even if outside
    the GC window

---
+++ internal/datastore/memdb/revisions_test.go
@@ -0,0 +1,27 @@
+package memdb
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeadRevision(t *testing.T) {
+	ds, err := NewMemdbDatastore(0, 0, 500*time.Millisecond)
+	require.NoError(t, err)
+
+	older, err := ds.HeadRevision(context.Background())
+	require.NoError(t, err)
+	err = ds.CheckRevision(context.Background(), older)
+	require.NoError(t, err)
+
+	time.Sleep(550 * time.Millisecond)
+
+	// GC window elapsed, last revision is returned even if outside GC window
+	newer, err := ds.HeadRevision(context.Background())
+	require.NoError(t, err)
+	err = ds.CheckRevision(context.Background(), newer)
+	require.NoError(t, err)
+}
