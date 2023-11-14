f7667fb98e0fee95aa5ad9870c0043f9964b1282
Víctor Roldán Betancort <vroldanbet@authzed.com>
2023-11-14 11:02:36 +0000

    rename field to better convey intent

--- pkg/graph/resolvermeta/resolvermeta.go
+++ pkg/graph/resolvermeta/resolvermeta.go
@@ -10,8 +10,8 @@ const defaultFalsePositiveRate = 0.001
 
 // NewTraversalBloomFilter creates a new bloom filter sized to the provided number of elements and
 // with a predefined false-positive ratio of 1%.
-func NewTraversalBloomFilter(depth uint) ([]byte, error) {
-	bf := bloom.NewWithEstimates(depth, defaultFalsePositiveRate)
+func NewTraversalBloomFilter(numElements uint) ([]byte, error) {
+	bf := bloom.NewWithEstimates(numElements, defaultFalsePositiveRate)
 
 	modifiedBloomFilter, err := bf.MarshalBinary()
 	if err != nil {
