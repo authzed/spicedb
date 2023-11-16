450b1f7f1adb546eccf05083cffc19e2f2679a2b
Joseph Schorr <josephschorr@users.noreply.github.com>
2023-11-16 16:58:15 -0500

    WIP: Try to remove PG GC flakes

--- internal/datastore/postgres/postgres_test.go
+++ internal/datastore/postgres/postgres_test.go
@@ -4,7 +4,11 @@
 package postgres
 
 import (
+	"fmt"
 	"testing"
+	"time"
+
+	testdatastore "github.com/authzed/spicedb/internal/testserver/datastore"
 )
 
 func TestPostgresDatastore(t *testing.T) {
@@ -17,4 +21,48 @@ func TestPostgresDatastoreWithoutCommitTimestamps(t *testing.T) {
 	t.Parallel()
 
 	testPostgresDatastoreWithoutCommitTimestamps(t, postgresConfigs)
+}
+
+func TestPostgresDatastoreGC(t *testing.T) {
+	for _, config := range postgresConfigs {
+		pgbouncerStr := ""
+		if config.pgbouncer {
+			pgbouncerStr = "pgbouncer-"
+		}
+		t.Run(fmt.Sprintf("%spostgres-gc-%s-%s-%s", pgbouncerStr, config.pgVersion, config.targetMigration, config.migrationPhase), func(t *testing.T) {
+			t.Parallel()
+
+			b := testdatastore.RunPostgresForTesting(t, "", config.targetMigration, config.pgVersion, config.pgbouncer)
+
+			t.Run("GarbageCollection", createDatastoreTest(
+				b,
+				GarbageCollectionTest,
+				RevisionQuantization(0),
+				GCWindow(1*time.Millisecond),
+				GCInterval(veryLargeGCInterval),
+				WatchBufferLength(1),
+				MigrationPhase(config.migrationPhase),
+			))
+
+			t.Run("GarbageCollectionByTime", createDatastoreTest(
+				b,
+				GarbageCollectionByTimeTest,
+				RevisionQuantization(0),
+				GCWindow(1*time.Millisecond),
+				GCInterval(veryLargeGCInterval),
+				WatchBufferLength(1),
+				MigrationPhase(config.migrationPhase),
+			))
+
+			t.Run("ChunkedGarbageCollection", createDatastoreTest(
+				b,
+				ChunkedGarbageCollectionTest,
+				RevisionQuantization(0),
+				GCWindow(1*time.Millisecond),
+				GCInterval(veryLargeGCInterval),
+				WatchBufferLength(1),
+				MigrationPhase(config.migrationPhase),
+			))
+		})
+	}
 }
