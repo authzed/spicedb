2b77bad34f7e86646a38e533818177587cb02dfc
Evan Cordell <cordell.evan@gmail.com>
2024-06-11 09:51:13 -0400

    spanner: use stale reads for current_timestamp for optimized revision
    The default is to use a strong read which does some coordination with
    the leader. For optimized revision computation, it should be fine to
    use the timestamp returned by the local spanner node and skip the extra
    coordination.
    
    Testing with a single-region spanner saw these queries drop an order
    of magnitude.
    
    It is probably also fine to use a stale read for the HeadRevision call,
    but holding off for further testing in case it could potentially cause
    a new enemy problem.

--- internal/datastore/spanner/revisions.go
+++ internal/datastore/spanner/revisions.go
@@ -11,28 +11,27 @@ import (
 	"github.com/authzed/spicedb/pkg/datastore"
 )
 
-var ParseRevisionString = revisions.RevisionParser(revisions.Timestamp)
-
-func (sd *spannerDatastore) headRevisionInternal(ctx context.Context) (datastore.Revision, error) {
-	now, err := sd.now(ctx)
-	if err != nil {
-		return datastore.NoRevision, fmt.Errorf(errRevision, err)
-	}
-
-	return revisions.NewForTime(now), nil
-}
+var (
+	ParseRevisionString = revisions.RevisionParser(revisions.Timestamp)
+	nowStmt             = spanner.NewStatement("SELECT CURRENT_TIMESTAMP()")
+)
 
 func (sd *spannerDatastore) HeadRevision(ctx context.Context) (datastore.Revision, error) {
-	return sd.headRevisionInternal(ctx)
-}
-
-func (sd *spannerDatastore) now(ctx context.Context) (time.Time, error) {
 	var timestamp time.Time
-	if err := sd.client.Single().Query(ctx, spanner.NewStatement("SELECT CURRENT_TIMESTAMP()")).Do(func(r *spanner.Row) error {
+	if err := sd.client.Single().Query(ctx, nowStmt).Do(func(r *spanner.Row) error {
 		return r.Columns(&timestamp)
 	}); err != nil {
-		return time.Time{}, err
+		return datastore.NoRevision, fmt.Errorf(errRevision, err)
 	}
+	return revisions.NewForTime(timestamp), nil
+}
 
-	return timestamp, nil
+func (sd *spannerDatastore) staleHeadRevision(ctx context.Context) (datastore.Revision, error) {
+	var timestamp time.Time
+	if err := sd.client.Single().WithTimestampBound(spanner.ExactStaleness(sd.config.followerReadDelay)).Query(ctx, nowStmt).Do(func(r *spanner.Row) error {
+		return r.Columns(&timestamp)
+	}); err != nil {
+		return datastore.NoRevision, fmt.Errorf(errRevision, err)
+	}
+	return revisions.NewForTime(timestamp), nil
 }
