822b3920ccf612d0b7e9d404edb36d880d67664b
Joseph Schorr <josephschorr@users.noreply.github.com>
2024-06-25 16:41:31 -0400

    Add basic LR steelthread tests

--- internal/services/steelthreadtesting/definitions.go
+++ internal/services/steelthreadtesting/definitions.go
@@ -62,4 +62,75 @@ var steelThreadTestCases = []steelThreadTestCase{
 			},
 		},
 	},
+	{
+		name:     "basic lookup resources",
+		datafile: "document-with-many-resources.yaml",
+		operations: []steelThreadOperationCase{
+			{
+				name:          "uncursored lookup resources for fred",
+				operationName: "lookupResources",
+				arguments: map[string]any{
+					"resource_type":     "document",
+					"permission":        "view",
+					"subject_type":      "user",
+					"subject_object_id": "fred",
+				},
+			},
+			{
+				name:          "cursored lookup resources for fred, page size 5",
+				operationName: "cursoredLookupResources",
+				arguments: map[string]any{
+					"resource_type":     "document",
+					"permission":        "view",
+					"subject_type":      "user",
+					"subject_object_id": "fred",
+					"page_size":         5,
+				},
+			},
+			{
+				name:          "cursored lookup resources for fred, page size 16",
+				operationName: "cursoredLookupResources",
+				arguments: map[string]any{
+					"resource_type":     "document",
+					"permission":        "view",
+					"subject_type":      "user",
+					"subject_object_id": "fred",
+					"page_size":         16,
+				},
+			},
+			{
+				name:          "cursored lookup resources for fred, page size 53",
+				operationName: "cursoredLookupResources",
+				arguments: map[string]any{
+					"resource_type":     "document",
+					"permission":        "view",
+					"subject_type":      "user",
+					"subject_object_id": "fred",
+					"page_size":         53,
+				},
+			},
+			{
+				name:          "cursored lookup resources for fred, page size 54",
+				operationName: "cursoredLookupResources",
+				arguments: map[string]any{
+					"resource_type":     "document",
+					"permission":        "view",
+					"subject_type":      "user",
+					"subject_object_id": "fred",
+					"page_size":         54,
+				},
+			},
+			{
+				name:          "cursored lookup resources for fred, page size 100",
+				operationName: "cursoredLookupResources",
+				arguments: map[string]any{
+					"resource_type":     "document",
+					"permission":        "view",
+					"subject_type":      "user",
+					"subject_object_id": "fred",
+					"page_size":         100,
+				},
+			},
+		},
+	},
 }
