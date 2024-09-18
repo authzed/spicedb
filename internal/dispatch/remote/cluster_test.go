543e6e047c41b8f7515965526fc78cd3b49410dc
Joseph Schorr <josephschorr@users.noreply.github.com>
2024-09-18 15:00:50 -0400

    Add support for secondary dispatching on LR2

--- internal/dispatch/remote/cluster_test.go
+++ internal/dispatch/remote/cluster_test.go
@@ -44,6 +44,23 @@ func (fds *fakeDispatchSvc) DispatchLookupSubjects(_ *v1.DispatchLookupSubjectsR
 	})
 }
 
+func (fds *fakeDispatchSvc) DispatchLookupResources2(_ *v1.DispatchLookupResources2Request, srv v1.DispatchService_DispatchLookupResources2Server) error {
+	if fds.dispatchCount == 999 {
+		return fmt.Errorf("error")
+	}
+
+	time.Sleep(fds.sleepTime)
+	return srv.Send(&v1.DispatchLookupResources2Response{
+		Metadata: &v1.ResponseMeta{
+			DispatchCount: fds.dispatchCount,
+		},
+		AfterResponseCursor: &v1.Cursor{
+			Sections:        nil,
+			DispatchVersion: 1,
+		},
+	})
+}
+
 func TestDispatchTimeout(t *testing.T) {
 	for _, tc := range []struct {
 		timeout   time.Duration
@@ -133,7 +150,7 @@ func TestDispatchTimeout(t *testing.T) {
 	}
 }
 
-func TestSecondaryDispatch(t *testing.T) {
+func TestCheckSecondaryDispatch(t *testing.T) {
 	for _, tc := range []struct {
 		name             string
 		expr             string
@@ -238,6 +255,282 @@ func TestSecondaryDispatch(t *testing.T) {
 			resp, err := dispatcher.DispatchCheck(context.Background(), tc.request)
 			require.NoError(t, err)
 			require.Equal(t, tc.expectedResult, resp.Metadata.DispatchCount)
+		})
+	}
+}
+
+func TestLRSecondaryDispatch(t *testing.T) {
+	for _, tc := range []struct {
+		name                  string
+		expr                  string
+		request               *v1.DispatchLookupResources2Request
+		expectedDispatchCount uint32
+		expectedError         bool
+	}{
+		{
+			"no multidispatch",
+			"['invalid']",
+			&v1.DispatchLookupResources2Request{
+				ResourceRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectIds: []string{"foo"},
+				TerminalSubject: &corev1.ObjectAndRelation{
+					Namespace: "foo",
+					ObjectId:  "bar",
+					Relation:  "...",
+				},
+				Metadata: &v1.ResolverMeta{DepthRemaining: 50},
+			},
+			1,
+			false,
+		},
+		{
+			"valid multidispatch",
+			"['secondary']",
+			&v1.DispatchLookupResources2Request{
+				ResourceRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectIds: []string{"foo"},
+				TerminalSubject: &corev1.ObjectAndRelation{
+					Namespace: "foo",
+					ObjectId:  "bar",
+					Relation:  "...",
+				},
+				Metadata: &v1.ResolverMeta{DepthRemaining: 50},
+			},
+			2,
+			false,
+		},
+		{
+			"cursored multidispatch to invalid secondary",
+			"['secondary']",
+			&v1.DispatchLookupResources2Request{
+				ResourceRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectIds: []string{"foo"},
+				TerminalSubject: &corev1.ObjectAndRelation{
+					Namespace: "foo",
+					ObjectId:  "bar",
+					Relation:  "...",
+				},
+				OptionalCursor: &v1.Cursor{
+					Sections:        []string{"somethingelse"},
+					DispatchVersion: 1,
+				},
+				Metadata: &v1.ResolverMeta{DepthRemaining: 50},
+			},
+			2, // Falls back to the default secondary.
+			false,
+		},
+		{
+			"cursored multidispatch to cursored secondary",
+			"['secondary']",
+			&v1.DispatchLookupResources2Request{
+				ResourceRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectIds: []string{"foo"},
+				TerminalSubject: &corev1.ObjectAndRelation{
+					Namespace: "foo",
+					ObjectId:  "bar",
+					Relation:  "...",
+				},
+				OptionalCursor: &v1.Cursor{
+					Sections:        []string{secondaryCursorPrefix + "tertiary"},
+					DispatchVersion: 1,
+				},
+				Metadata: &v1.ResolverMeta{DepthRemaining: 50},
+			},
+			3,
+			false,
+		},
+		{
+			"cursored multidispatch to cursored secondary that raises an error",
+			"['secondary']",
+			&v1.DispatchLookupResources2Request{
+				ResourceRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectIds: []string{"foo"},
+				TerminalSubject: &corev1.ObjectAndRelation{
+					Namespace: "foo",
+					ObjectId:  "bar",
+					Relation:  "...",
+				},
+				OptionalCursor: &v1.Cursor{
+					Sections:        []string{secondaryCursorPrefix + "error"},
+					DispatchVersion: 1,
+				},
+				Metadata: &v1.ResolverMeta{DepthRemaining: 50},
+			},
+			1,
+			true, // since the secondary was in the cursor, if it errors, the operation fails.
+		},
+		{
+			"cursored multidispatch to default secondary that raises an error",
+			"['error']",
+			&v1.DispatchLookupResources2Request{
+				ResourceRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectIds: []string{"foo"},
+				TerminalSubject: &corev1.ObjectAndRelation{
+					Namespace: "foo",
+					ObjectId:  "bar",
+					Relation:  "...",
+				},
+				Metadata: &v1.ResolverMeta{DepthRemaining: 50},
+			},
+			1,
+			false,
+		},
+		{
+			"cursored multidispatch to unknown secondary",
+			"['error']",
+			&v1.DispatchLookupResources2Request{
+				ResourceRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectIds: []string{"foo"},
+				TerminalSubject: &corev1.ObjectAndRelation{
+					Namespace: "foo",
+					ObjectId:  "bar",
+					Relation:  "...",
+				},
+				OptionalCursor: &v1.Cursor{
+					Sections:        []string{secondaryCursorPrefix + "unknown"},
+					DispatchVersion: 1,
+				},
+				Metadata: &v1.ResolverMeta{DepthRemaining: 50},
+			},
+			0,
+			true,
+		},
+		{
+			"cursored multidispatch to default secondary",
+			"['secondary', 'tertiary']",
+			&v1.DispatchLookupResources2Request{
+				ResourceRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectIds: []string{"foo"},
+				TerminalSubject: &corev1.ObjectAndRelation{
+					Namespace: "foo",
+					ObjectId:  "bar",
+					Relation:  "...",
+				},
+				OptionalCursor: &v1.Cursor{
+					Sections:        []string{secondaryCursorPrefix + "secondary"},
+					DispatchVersion: 1,
+				},
+				Metadata: &v1.ResolverMeta{DepthRemaining: 50},
+			},
+			2,
+			false,
+		},
+		{
+			"cursored multidispatch to non-default secondary",
+			"['tertiary', 'secondary']",
+			&v1.DispatchLookupResources2Request{
+				ResourceRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectRelation: &corev1.RelationReference{
+					Namespace: "somenamespace",
+					Relation:  "somerelation",
+				},
+				SubjectIds: []string{"foo"},
+				TerminalSubject: &corev1.ObjectAndRelation{
+					Namespace: "foo",
+					ObjectId:  "bar",
+					Relation:  "...",
+				},
+				OptionalCursor: &v1.Cursor{
+					Sections:        []string{secondaryCursorPrefix + "secondary"},
+					DispatchVersion: 1,
+				},
+				Metadata: &v1.ResolverMeta{DepthRemaining: 50},
+			},
+			2,
+			false,
+		},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			conn := connectionForDispatching(t, &fakeDispatchSvc{dispatchCount: 1, sleepTime: 0 * time.Millisecond})
+			secondaryConn := connectionForDispatching(t, &fakeDispatchSvc{dispatchCount: 2, sleepTime: 0 * time.Millisecond})
+			tertiaryConn := connectionForDispatching(t, &fakeDispatchSvc{dispatchCount: 3, sleepTime: 0 * time.Millisecond})
+			errorConn := connectionForDispatching(t, &fakeDispatchSvc{dispatchCount: 999, sleepTime: 0 * time.Millisecond})
+
+			parsed, err := ParseDispatchExpression("lookupresources", tc.expr)
+			require.NoError(t, err)
+
+			dispatcher := NewClusterDispatcher(v1.NewDispatchServiceClient(conn), conn, ClusterDispatcherConfig{
+				KeyHandler:             &keys.DirectKeyHandler{},
+				DispatchOverallTimeout: 30 * time.Second,
+			}, map[string]SecondaryDispatch{
+				"secondary": {Name: "secondary", Client: v1.NewDispatchServiceClient(secondaryConn)},
+				"tertiary":  {Name: "tertiary", Client: v1.NewDispatchServiceClient(tertiaryConn)},
+				"error":     {Name: "error", Client: v1.NewDispatchServiceClient(errorConn)},
+			}, map[string]*DispatchExpr{
+				"lookupresources": parsed,
+			})
+			require.True(t, dispatcher.ReadyState().IsReady)
+
+			stream := dispatch.NewCollectingDispatchStream[*v1.DispatchLookupResources2Response](context.Background())
+			err = dispatcher.DispatchLookupResources2(tc.request, stream)
+
+			if tc.expectedError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, 1, len(stream.Results()))
+				require.Equal(t, tc.expectedDispatchCount, stream.Results()[0].Metadata.DispatchCount)
+			}
 		})
 	}
 }
