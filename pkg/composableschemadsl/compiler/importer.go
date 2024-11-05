56a5c2474990ef80db1472e721607b64729049eb
Tanner Stirrat <tstirrat@gmail.com>
2024-11-05 16:09:36 -0700

    Implement circular import detection + import dedup

--- pkg/composableschemadsl/compiler/importer.go
+++ pkg/composableschemadsl/compiler/importer.go
@@ -13,12 +13,19 @@ import (
 )
 
 type importContext struct {
-	pathSegments []string
-	sourceFolder string
-	names        *mapz.Set[string]
+	pathSegments         []string
+	sourceFolder         string
+	names                *mapz.Set[string]
+	locallyVisitedFiles  *mapz.Set[string]
+	globallyVisitedFiles *mapz.Set[string]
 }
 
 const SchemaFileSuffix = ".zed"
+
+type ErrCircularImport struct {
+	error
+	filePath string
+}
 
 func importFile(importContext importContext) (*CompiledSchema, error) {
 	relativeFilepath := constructFilePath(importContext.pathSegments)
@@ -26,25 +33,55 @@ func importFile(importContext importContext) (*CompiledSchema, error) {
 
 	newSourceFolder := filepath.Dir(filePath)
 
-	var schemaBytes []byte
+	currentLocallyVisitedFiles := importContext.locallyVisitedFiles.Copy()
+
+	if ok := currentLocallyVisitedFiles.Add(filePath); !ok {
+		// If we've already visited the file on this particular branch walk, it's
+		// a circular import issue.
+		return nil, &ErrCircularImport{
+			error:    fmt.Errorf("circular import detected: %s has been visited on this branch", filePath),
+			filePath: filePath,
+		}
+	}
+
+	if ok := importContext.globallyVisitedFiles.Add(filePath); !ok {
+		// If the file has already been visited, we short-circuit the import process
+		// by not reading the schema file in and compiling a schema with an empty string.
+		// This prevents duplicate definitions from ending up in the output, as well
+		// as preventing circular imports.
+		log.Debug().Str("filepath", filePath).Msg("file %s has already been visited in another part of the walk")
+		return compileImpl(InputSchema{
+			Source:       input.Source(filePath),
+			SchemaString: "",
+		},
+			compilationContext{
+				existingNames:        importContext.names,
+				locallyVisitedFiles:  currentLocallyVisitedFiles,
+				globallyVisitedFiles: importContext.globallyVisitedFiles,
+			},
+			AllowUnprefixedObjectType(),
+			SourceFolder(newSourceFolder),
+		)
+	}
+
 	schemaBytes, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read schema file: %w", err)
 	}
 	log.Trace().Str("schema", string(schemaBytes)).Str("file", filePath).Msg("read schema from file")
 
-	compiled, err := compileImpl(InputSchema{
+	return compileImpl(InputSchema{
 		Source:       input.Source(filePath),
 		SchemaString: string(schemaBytes),
 	},
-		importContext.names,
+		compilationContext{
+			existingNames:        importContext.names,
+			locallyVisitedFiles:  currentLocallyVisitedFiles,
+			globallyVisitedFiles: importContext.globallyVisitedFiles,
+		},
 		AllowUnprefixedObjectType(),
 		SourceFolder(newSourceFolder),
 	)
-	if err != nil {
-		return nil, err
-	}
-	return compiled, nil
 }
 
 func constructFilePath(segments []string) string {
