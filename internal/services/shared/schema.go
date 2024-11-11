a1c3d641b35d6676d149ef95eb33512cede6c46b
Joseph Schorr <josephschorr@users.noreply.github.com>
2024-11-11 12:21:48 -0500

    Add missing limit in schema delta checking

--- internal/services/shared/schema.go
+++ internal/services/shared/schema.go
@@ -322,7 +322,7 @@ func sanityCheckNamespaceChanges(
 			qy, qyErr := rwt.QueryRelationships(ctx, datastore.RelationshipsFilter{
 				OptionalResourceType:     nsdef.Name,
 				OptionalResourceRelation: delta.RelationName,
-			})
+			}, options.WithLimit(options.LimitOne))
 
 			err = errorIfTupleIteratorReturnsTuples(
 				ctx,
