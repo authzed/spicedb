c005116068aaeb529b7adb8489d4538af137bf29
ivancherepov <cherapau@gmail.com>
2024-12-05 12:25:32 -0500

    remove internal/datastore/postgres/log_tracer.go:13:25: unreachable func: tracingLogger.Log

--- internal/datastore/postgres/log_tracer.go
+++ internal/datastore/postgres/log_tracer.go
@@ -1,16 +1,3 @@
 package postgres
 
-import (
-	"context"
-
-	"github.com/jackc/pgx/v5/tracelog"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/trace"
-)
-
 type tracingLogger struct{}
-
-func (tl tracingLogger) Log(ctx context.Context, level tracelog.LogLevel, msg string, _ map[string]interface{}) {
-	span := trace.SpanFromContext(ctx)
-	span.AddEvent(msg, trace.WithAttributes(attribute.Stringer("level", level), attribute.String("datastore", "postgres")))
-}
