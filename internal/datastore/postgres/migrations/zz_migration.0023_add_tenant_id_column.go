2a8060b5c581cbdcbcb65178a24da055f6117be3
viqueen <hasnae.rehioui@safetyculture.io>
2025-01-20 21:45:29 +1100

    add index on tenant id

--- internal/datastore/postgres/migrations/zz_migration.0023_add_tenant_id_column.go
+++ internal/datastore/postgres/migrations/zz_migration.0023_add_tenant_id_column.go
@@ -6,6 +6,9 @@ import (
 )
 
 const addTenantIDColumnToRelationTupleTable = `ALTER TABLE relation_tuple ADD COLUMN IF NOT EXISTS tenant_id VARCHAR NOT NULL DEFAULT '';`
+const createIndexForRelationTupleTenantID = `CREATE INDEX CONCURRENTLY 
+	IF NOT EXISTS idx_relation_tuple_tenant_id 
+	ON relation_tuple (tenant_id);`
 
 func init() {
 	err := DatabaseMigrations.Register(
@@ -14,6 +17,9 @@ func init() {
 		noNonatomicMigration,
 		func(ctx context.Context, tx pgx.Tx) error {
 			if _, err := tx.Exec(ctx, addTenantIDColumnToRelationTupleTable); err != nil {
+				return err
+			}
+			if _, err := tx.Exec(ctx, createIndexForRelationTupleTenantID); err != nil {
 				return err
 			}
 			return nil
