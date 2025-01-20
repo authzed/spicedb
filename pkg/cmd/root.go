e7ec237dd6d9ed27af09a36e7e44b18a8b9a977b
Kartikay <kartikay_2101ce32@iitp.ac.in>
2025-01-20 22:47:05 +0530

    docs for spicedb
    Signed-off-by: Kartikay <kartikay_2101ce32@iitp.ac.in>

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/jzelinskie/cobrautil/v2"
 	"github.com/jzelinskie/cobrautil/v2/cobrazerolog"
@@ -9,8 +11,98 @@ import (
 
 	log "github.com/authzed/spicedb/internal/logging"
 	"github.com/authzed/spicedb/pkg/cmd/server"
+	"github.com/authzed/spicedb/pkg/cmd/testserver"
 	"github.com/authzed/spicedb/pkg/releases"
+	mcobra "github.com/muesli/mango-cobra"
+	"github.com/muesli/roff"
 )
+
+var ErrParsing = errors.New("parsing error")
+
+// This function is utilised to generate docs for SpiceDB
+func InitialiseRootCmd() *cobra.Command {
+	rootCmd := NewRootCommand("spicedb")
+	rootCmd.SetFlagErrorFunc(func(cmd *cobra.Command, err error) error {
+		cmd.Println(err)
+		cmd.Println(cmd.UsageString())
+		return ErrParsing
+	})
+	if err := RegisterRootFlags(rootCmd); err != nil {
+		log.Fatal().Err(err).Msg("failed to register root flags")
+	}
+
+	// Add a version command
+	versionCmd := NewVersionCommand(rootCmd.Use)
+	RegisterVersionFlags(versionCmd)
+	rootCmd.AddCommand(versionCmd)
+
+	// Add datastore commands
+	datastoreCmd, err := NewDatastoreCommand(rootCmd.Use)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to register datastore command")
+	}
+
+	RegisterDatastoreRootFlags(datastoreCmd)
+	rootCmd.AddCommand(datastoreCmd)
+
+	// Add deprecated head command
+	headCmd := NewHeadCommand(rootCmd.Use)
+	RegisterHeadFlags(headCmd)
+	headCmd.Hidden = true
+	headCmd.RunE = DeprecatedRunE(headCmd.RunE, "spicedb datastore head")
+	rootCmd.AddCommand(headCmd)
+
+	// Add deprecated migrate command
+	migrateCmd := NewMigrateCommand(rootCmd.Use)
+	migrateCmd.Hidden = true
+	migrateCmd.RunE = DeprecatedRunE(migrateCmd.RunE, "spicedb datastore migrate")
+	RegisterMigrateFlags(migrateCmd)
+	rootCmd.AddCommand(migrateCmd)
+
+	// Add server commands
+	serverConfig := server.NewConfigWithOptionsAndDefaults()
+	serveCmd := NewServeCommand(rootCmd.Use, serverConfig)
+	if err := RegisterServeFlags(serveCmd, serverConfig); err != nil {
+		log.Fatal().Err(err).Msg("failed to register server flags")
+	}
+	rootCmd.AddCommand(serveCmd)
+
+	devtoolsCmd := NewDevtoolsCommand(rootCmd.Use)
+	RegisterDevtoolsFlags(devtoolsCmd)
+	rootCmd.AddCommand(devtoolsCmd)
+
+	lspConfig := new(LSPConfig)
+	lspCmd := NewLSPCommand(rootCmd.Use, lspConfig)
+	if err := RegisterLSPFlags(lspCmd, lspConfig); err != nil {
+		log.Fatal().Err(err).Msg("failed to register lsp flags")
+	}
+	rootCmd.AddCommand(lspCmd)
+
+	var testServerConfig testserver.Config
+	testingCmd := NewTestingCommand(rootCmd.Use, &testServerConfig)
+	RegisterTestingFlags(testingCmd, &testServerConfig)
+	rootCmd.AddCommand(testingCmd)
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:                   "man",
+		Short:                 "Generate the SpiceDB manpage",
+		SilenceUsage:          true,
+		DisableFlagsInUseLine: true,
+		Hidden:                true,
+		Args:                  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			manPage, err := mcobra.NewManPage(1, cmd.Root())
+			if err != nil {
+				return err
+			}
+
+			_, err = fmt.Fprint(os.Stdout, manPage.Build(roff.NewDocument()))
+			return err
+		},
+	})
+
+	return rootCmd
+}
 
 func RegisterRootFlags(cmd *cobra.Command) error {
 	zl := cobrazerolog.New()
