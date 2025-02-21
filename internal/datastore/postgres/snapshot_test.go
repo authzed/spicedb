fb3dc2a1339421589eeb361f4c545b4b54334ab3
Víctor Roldán Betancort <vroldanbet@authzed.com>
2025-02-21 16:17:15 +0000

    expand pgSnapshot tests

--- internal/datastore/postgres/snapshot_test.go
+++ internal/datastore/postgres/snapshot_test.go
@@ -56,6 +56,7 @@ func TestMarkComplete(t *testing.T) {
 		{snap(3, 5, 3), 3, snap(5, 5)},
 		{snap(0, 0), 5, snap(0, 6, 0, 1, 2, 3, 4)},
 		{snap(5, 5), 4, snap(5, 5)},
+		{snap(3, 5, 4), 5, snap(4, 6, 4)},
 	}
 
 	for _, tc := range testCases {
@@ -64,6 +65,55 @@ func TestMarkComplete(t *testing.T) {
 			require := require.New(t)
 			completed := tc.snapshot.markComplete(tc.toMark)
 			require.Equal(tc.expected, completed, "%s != %s", tc.expected, completed)
+		})
+	}
+}
+
+func TestVisible(t *testing.T) {
+	testCases := []struct {
+		snapshot pgSnapshot
+		txID     uint64
+		visible  bool
+	}{
+		{snap(840, 842, 840), 841, true},
+		{snap(840, 842, 840), 840, false},
+		{snap(840, 842, 840), 842, false},
+		{snap(840, 842, 840), 839, true},
+	}
+
+	f := func(b bool) string {
+		if b {
+			return "visible"
+		}
+		return "not visible"
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(fmt.Sprintf("%d %s %s", tc.txID, f(tc.visible)+" in", tc.snapshot), func(t *testing.T) {
+			require := require.New(t)
+			result := tc.snapshot.txVisible(tc.txID)
+			require.Equal(tc.visible, result, "expected %s but got %s", f(tc.visible), f(result))
+		})
+	}
+}
+
+func TestCompare(t *testing.T) {
+	testCases := []struct {
+		snapshot    pgSnapshot
+		compareWith pgSnapshot
+		result      comparisonResult
+	}{
+		{snap(0, 4, 2), snap(0, 4, 2, 3), gt},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("%s %s %s", tc.snapshot, tc.result, tc.compareWith), func(t *testing.T) {
+			require := require.New(t)
+			result := tc.snapshot.compare(tc.compareWith)
+			require.Equal(tc.result, result, "expected %s got %s", tc.result, result)
 		})
 	}
 }
