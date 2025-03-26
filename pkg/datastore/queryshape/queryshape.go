5aacd850e56037ef3f0681bbb9086bd022cc3f9e
Joseph Schorr <josephschorr@users.noreply.github.com>
2025-03-26 15:19:44 +0200

    Start on queryshape validation during index checking

--- pkg/datastore/queryshape/queryshape.go
+++ pkg/datastore/queryshape/queryshape.go
@@ -13,9 +13,16 @@ const (
 
 	// CheckPermissionSelectDirectSubjects indicates that the query is a permission check
 	// that selects direct subjects.
+	//
+	// The query shape selects a specific relationship based on filling in *all* of it
+	// relationship fields (except the caveat name, context and expiration).
 	CheckPermissionSelectDirectSubjects = "check-permission-select-direct-subjects"
 
 	// CheckPermissionSelectIndirectSubjects indicates that the query is a permission check
 	// that selects indirect subjects.
+	//
+	// The query shape selects a specific relationship based on filling in all of the fields
+	// on the relationship except the subject relation, which is set to match any non-...
+	// relations.
 	CheckPermissionSelectIndirectSubjects = "check-permission-select-indirect-subjects"
 )
