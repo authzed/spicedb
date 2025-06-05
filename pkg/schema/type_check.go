0853f458a6e9a8d351d6f0d21fb8bfb819f9b84f
Barak Michener <barak.michener@authzed.com>
2025-06-05 16:42:08 -0700

    fix arrows-over-subtype in type checking

--- pkg/schema/type_check.go
+++ pkg/schema/type_check.go
@@ -14,7 +14,7 @@ const ellipsesRelation = "..."
 // terminal subject type definition names of that relation.
 func (ts *TypeSystem) GetRecursiveTerminalTypesForRelation(ctx context.Context, defName string, relationName string) ([]string, error) {
 	seen := mapz.NewSet[string]()
-	set, err := ts.getTypesForRelationInternal(ctx, defName, relationName, seen, false)
+	set, err := ts.getTypesForRelationInternal(ctx, defName, relationName, seen, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -25,14 +25,16 @@ func (ts *TypeSystem) GetRecursiveTerminalTypesForRelation(ctx context.Context, 
 // terminal subject type definition names of that relation, as well as any relation subtypes (eg, `group#member`) that may occur.
 func (ts *TypeSystem) GetFullRecursiveSubjectTypesForRelation(ctx context.Context, defName string, relationName string) ([]string, error) {
 	seen := mapz.NewSet[string]()
-	set, err := ts.getTypesForRelationInternal(ctx, defName, relationName, seen, true)
+	nonTerminals := mapz.NewSet[string]()
+	set, err := ts.getTypesForRelationInternal(ctx, defName, relationName, seen, nonTerminals)
 	if err != nil {
 		return nil, err
 	}
+	set.Merge(nonTerminals)
 	return set.AsSlice(), nil
 }
 
-func (ts *TypeSystem) getTypesForRelationInternal(ctx context.Context, defName string, relationName string, seen *mapz.Set[string], addNonTerminals bool) (*mapz.Set[string], error) {
+func (ts *TypeSystem) getTypesForRelationInternal(ctx context.Context, defName string, relationName string, seen *mapz.Set[string], nonTerminals *mapz.Set[string]) (*mapz.Set[string], error) {
 	id := fmt.Sprint(defName, "#", relationName)
 	if seen.Has(id) {
 		return nil, nil
@@ -47,25 +49,28 @@ func (ts *TypeSystem) getTypesForRelationInternal(ctx context.Context, defName s
 		return nil, asTypeError(NewRelationNotFoundErr(defName, relationName))
 	}
 	if rel.TypeInformation != nil {
-		return ts.getTypesForInfo(ctx, defName, rel.TypeInformation, seen, addNonTerminals)
+		return ts.getTypesForInfo(ctx, rel.TypeInformation, seen, nonTerminals)
 	} else if rel.UsersetRewrite != nil {
-		return ts.getTypesForRewrite(ctx, defName, rel.UsersetRewrite, seen, addNonTerminals)
+		return ts.getTypesForRewrite(ctx, defName, rel.UsersetRewrite, seen, nonTerminals)
 	}
 	return nil, asTypeError(NewMissingAllowedRelationsErr(defName, relationName))
 }
 
-func (ts *TypeSystem) getTypesForInfo(ctx context.Context, defName string, rel *corev1.TypeInformation, seen *mapz.Set[string], addNonTerminals bool) (*mapz.Set[string], error) {
+func (ts *TypeSystem) getTypesForInfo(ctx context.Context, rel *corev1.TypeInformation, seen *mapz.Set[string], nonTerminals *mapz.Set[string]) (*mapz.Set[string], error) {
 	out := mapz.NewSet[string]()
 	for _, dr := range rel.GetAllowedDirectRelations() {
 		if dr.GetRelation() == ellipsesRelation {
 			out.Add(dr.GetNamespace())
 		} else if dr.GetRelation() != "" {
-			if addNonTerminals {
-				out.Add(fmt.Sprintf("%s#%s", dr.GetNamespace(), dr.GetRelation()))
+			if nonTerminals != nil {
+				nonTerminals.Add(fmt.Sprintf("%s#%s", dr.GetNamespace(), dr.GetRelation()))
 			}
-			rest, err := ts.getTypesForRelationInternal(ctx, dr.GetNamespace(), dr.GetRelation(), seen, addNonTerminals)
+			rest, err := ts.getTypesForRelationInternal(ctx, dr.GetNamespace(), dr.GetRelation(), seen, nonTerminals)
 			if err != nil {
 				return nil, err
+			}
+			if rest == nil {
+				continue
 			}
 			out.Merge(rest)
 		} else {
@@ -76,7 +81,23 @@ func (ts *TypeSystem) getTypesForInfo(ctx context.Context, defName string, rel *
 	return out, nil
 }
 
-func (ts *TypeSystem) getTypesForRewrite(ctx context.Context, defName string, rel *corev1.UsersetRewrite, seen *mapz.Set[string], addNonTerminals bool) (*mapz.Set[string], error) {
+func (ts *TypeSystem) getIntermediateRelationTypes(ctx context.Context, defName string, relName string) (*mapz.Set[string], error) {
+	def, err := ts.GetDefinition(ctx, defName)
+	if err != nil {
+		return nil, err
+	}
+	rel, ok := def.GetRelation(relName)
+	if !ok {
+		return nil, asTypeError(NewRelationNotFoundErr(defName, relName))
+	}
+	out := mapz.NewSet[string]()
+	for _, dr := range rel.TypeInformation.GetAllowedDirectRelations() {
+		out.Add(dr.GetNamespace())
+	}
+	return out, nil
+}
+
+func (ts *TypeSystem) getTypesForRewrite(ctx context.Context, defName string, rel *corev1.UsersetRewrite, seen *mapz.Set[string], nonTerminals *mapz.Set[string]) (*mapz.Set[string], error) {
 	out := mapz.NewSet[string]()
 
 	// We're finding the union of all the things touched, regardless.
@@ -88,30 +109,29 @@ func (ts *TypeSystem) getTypesForRewrite(ctx context.Context, defName string, re
 		}
 		for _, child := range op.GetChild() {
 			if computed := child.GetComputedUserset(); computed != nil {
-				set, err := ts.getTypesForRelationInternal(ctx, defName, computed.GetRelation(), seen, addNonTerminals)
+				set, err := ts.getTypesForRelationInternal(ctx, defName, computed.GetRelation(), seen, nonTerminals)
 				if err != nil {
 					return nil, err
+				}
+				if set == nil {
+					continue
 				}
 				out.Merge(set)
 			}
 			if rewrite := child.GetUsersetRewrite(); rewrite != nil {
-				sub, err := ts.getTypesForRewrite(ctx, defName, rewrite, seen, addNonTerminals)
+				sub, err := ts.getTypesForRewrite(ctx, defName, rewrite, seen, nonTerminals)
 				if err != nil {
 					return nil, err
 				}
 				out.Merge(sub)
 			}
 			if userset := child.GetTupleToUserset(); userset != nil {
-				set, err := ts.getTypesForRelationInternal(ctx, defName, userset.GetTupleset().GetRelation(), seen, addNonTerminals)
+				set, err := ts.getIntermediateRelationTypes(ctx, defName, userset.GetTupleset().GetRelation())
 				if err != nil {
 					return nil, err
 				}
-				if set == nil {
-					// We've already seen it.
-					continue
-				}
 				for _, s := range set.AsSlice() {
-					targets, err := ts.getTypesForRelationInternal(ctx, s, userset.GetComputedUserset().GetRelation(), seen, addNonTerminals)
+					targets, err := ts.getTypesForRelationInternal(ctx, s, userset.GetComputedUserset().GetRelation(), seen, nonTerminals)
 					if err != nil {
 						return nil, err
 					}
@@ -123,21 +143,17 @@ func (ts *TypeSystem) getTypesForRewrite(ctx context.Context, defName string, re
 				}
 			}
 			if functioned := child.GetFunctionedTupleToUserset(); functioned != nil {
-				set, err := ts.getTypesForRelationInternal(ctx, defName, functioned.GetTupleset().GetRelation(), seen, addNonTerminals)
+				set, err := ts.getIntermediateRelationTypes(ctx, defName, functioned.GetTupleset().GetRelation())
 				if err != nil {
 					return nil, err
 				}
-				if set == nil {
-					// We've already seen it.
-					continue
-				}
 				for _, s := range set.AsSlice() {
-					targets, err := ts.getTypesForRelationInternal(ctx, s, functioned.GetComputedUserset().GetRelation(), seen, addNonTerminals)
-					if targets == nil {
-						continue
-					}
+					targets, err := ts.getTypesForRelationInternal(ctx, s, functioned.GetComputedUserset().GetRelation(), seen, nonTerminals)
 					if err != nil {
 						return nil, err
+					}
+					if targets == nil {
+						continue
 					}
 					out.Merge(targets)
 				}
