4afa98e79096d734f894e854eeb8e688b0d22175
Joseph Schorr <josephschorr@users.noreply.github.com>
2025-06-09 15:52:13 -0400

    Add further filtering of forced indexes on CRDB to discount other shapes

--- internal/datastore/crdb/schema/indexes_test.go
+++ internal/datastore/crdb/schema/indexes_test.go
@@ -11,19 +11,22 @@ import (
 
 func TestIndexForFilter(t *testing.T) {
 	tests := []struct {
-		name     string
-		filter   datastore.RelationshipsFilter
-		expected string
+		name                     string
+		filter                   datastore.RelationshipsFilter
+		expectedWithoutIntegrity string
+		expectedWithIntegrity    string
 	}{
 		{
-			name:     "no filter",
-			filter:   datastore.RelationshipsFilter{},
-			expected: "",
+			name:                     "no filter",
+			filter:                   datastore.RelationshipsFilter{},
+			expectedWithoutIntegrity: "",
+			expectedWithIntegrity:    "",
 		},
 		{
-			name:     "filter by resource type",
-			filter:   datastore.RelationshipsFilter{OptionalResourceType: "foo"},
-			expected: "pk_relation_tuple",
+			name:                     "filter by resource type",
+			filter:                   datastore.RelationshipsFilter{OptionalResourceType: "foo"},
+			expectedWithoutIntegrity: "pk_relation_tuple",
+			expectedWithIntegrity:    "pk_relation_tuple",
 		},
 		{
 			name: "filter by resource type and relation",
@@ -31,7 +34,8 @@ func TestIndexForFilter(t *testing.T) {
 				OptionalResourceType:     "foo",
 				OptionalResourceRelation: "bar",
 			},
-			expected: "pk_relation_tuple",
+			expectedWithoutIntegrity: "pk_relation_tuple",
+			expectedWithIntegrity:    "pk_relation_tuple",
 		},
 		{
 			name: "filter by resource type, resource ID and relation",
@@ -40,7 +44,8 @@ func TestIndexForFilter(t *testing.T) {
 				OptionalResourceIds:      []string{"baz"},
 				OptionalResourceRelation: "bar",
 			},
-			expected: "pk_relation_tuple",
+			expectedWithoutIntegrity: "pk_relation_tuple",
+			expectedWithIntegrity:    "pk_relation_tuple",
 		},
 		{
 			name: "filter by subject type, subject ID and relation",
@@ -55,7 +60,8 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "ix_relation_tuple_by_subject",
+			expectedWithoutIntegrity: "ix_relation_tuple_by_subject",
+			expectedWithIntegrity:    "",
 		},
 		{
 			name: "filter by subject type, subject ID",
@@ -67,7 +73,8 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "ix_relation_tuple_by_subject",
+			expectedWithoutIntegrity: "ix_relation_tuple_by_subject",
+			expectedWithIntegrity:    "",
 		},
 		{
 			name: "filter by subject relation, subject ID",
@@ -81,7 +88,8 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "ix_relation_tuple_by_subject",
+			expectedWithoutIntegrity: "ix_relation_tuple_by_subject",
+			expectedWithIntegrity:    "",
 		},
 		{
 			name: "filter by subject type",
@@ -92,7 +100,8 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "",
+			expectedWithoutIntegrity: "",
+			expectedWithIntegrity:    "",
 		},
 		{
 			name: "filter by resource type and subject type",
@@ -104,7 +113,8 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "pk_relation_tuple",
+			expectedWithoutIntegrity: "pk_relation_tuple",
+			expectedWithIntegrity:    "pk_relation_tuple",
 		},
 		{
 			name: "filter by resource type and subject object ID",
@@ -116,7 +126,8 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "",
+			expectedWithoutIntegrity: "",
+			expectedWithIntegrity:    "",
 		},
 		{
 			name: "filter by resource type, relation and subject type and relation",
@@ -132,7 +143,8 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "ix_relation_tuple_by_subject_relation",
+			expectedWithoutIntegrity: "ix_relation_tuple_by_subject_relation",
+			expectedWithIntegrity:    "ix_relation_tuple_by_subject_relation",
 		},
 		{
 			name: "filter by resource type, relation and subject type",
@@ -145,7 +157,8 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "pk_relation_tuple",
+			expectedWithoutIntegrity: "pk_relation_tuple",
+			expectedWithIntegrity:    "pk_relation_tuple",
 		},
 		{
 			name: "filter by resource type, relation and subject relation",
@@ -160,7 +173,8 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "pk_relation_tuple",
+			expectedWithoutIntegrity: "pk_relation_tuple",
+			expectedWithIntegrity:    "pk_relation_tuple",
 		},
 		{
 			name: "filter by resource relation and subject type and relation",
@@ -175,7 +189,8 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "",
+			expectedWithoutIntegrity: "",
+			expectedWithIntegrity:    "",
 		},
 		{
 			name: "filter by resource type, relation and subject type and relation, include ellipsis",
@@ -192,20 +207,149 @@ func TestIndexForFilter(t *testing.T) {
 					},
 				},
 			},
-			expected: "pk_relation_tuple",
+			expectedWithoutIntegrity: "pk_relation_tuple",
+			expectedWithIntegrity:    "pk_relation_tuple",
+		},
+		{
+			name: "filter by resource type and ID prefix",
+			filter: datastore.RelationshipsFilter{
+				OptionalResourceType:     "foo",
+				OptionalResourceIDPrefix: "prefix",
+			},
+			expectedWithoutIntegrity: "pk_relation_tuple",
+			expectedWithIntegrity:    "pk_relation_tuple",
+		},
+		{
+			name: "filter by resource type, ID prefix and relation",
+			filter: datastore.RelationshipsFilter{
+				OptionalResourceType:     "foo",
+				OptionalResourceIDPrefix: "prefix",
+				OptionalResourceRelation: "bar",
+			},
+			expectedWithoutIntegrity: "",
+			expectedWithIntegrity:    "",
+		},
+		{
+			name: "multiple subject selectors with different depths",
+			filter: datastore.RelationshipsFilter{
+				OptionalSubjectsSelectors: []datastore.SubjectsSelector{
+					{
+						OptionalSubjectType: "foo",
+					},
+					{
+						OptionalSubjectType: "bar",
+						OptionalSubjectIds:  []string{"id1"},
+						RelationFilter: datastore.SubjectRelationFilter{
+							NonEllipsisRelation: "baz",
+						},
+					},
+				},
+			},
+			expectedWithoutIntegrity: "",
+			expectedWithIntegrity:    "",
+		},
+		{
+			name: "multiple subject selectors with same depth",
+			filter: datastore.RelationshipsFilter{
+				OptionalSubjectsSelectors: []datastore.SubjectsSelector{
+					{
+						OptionalSubjectType: "foo",
+						OptionalSubjectIds:  []string{"id1"},
+					},
+					{
+						OptionalSubjectType: "bar",
+						OptionalSubjectIds:  []string{"id2"},
+					},
+				},
+			},
+			expectedWithoutIntegrity: "ix_relation_tuple_by_subject",
+			expectedWithIntegrity:    "",
+		},
+		{
+			name: "multiple subject selectors with resource filter",
+			filter: datastore.RelationshipsFilter{
+				OptionalResourceType: "resource_type",
+				OptionalSubjectsSelectors: []datastore.SubjectsSelector{
+					{
+						OptionalSubjectType: "foo",
+					},
+					{
+						OptionalSubjectType: "bar",
+						OptionalSubjectIds:  []string{"id1"},
+					},
+				},
+			},
+			expectedWithoutIntegrity: "",
+			expectedWithIntegrity:    "",
+		},
+		{
+			name: "subject IDs without subject type",
+			filter: datastore.RelationshipsFilter{
+				OptionalSubjectsSelectors: []datastore.SubjectsSelector{
+					{
+						OptionalSubjectIds: []string{"id1"},
+						RelationFilter: datastore.SubjectRelationFilter{
+							NonEllipsisRelation: "relation",
+						},
+					},
+				},
+			},
+			expectedWithoutIntegrity: "ix_relation_tuple_by_subject",
+			expectedWithIntegrity:    "",
+		},
+		{
+			name: "schema diff index with only non-ellipsis relations",
+			filter: datastore.RelationshipsFilter{
+				OptionalResourceType:     "foo",
+				OptionalResourceRelation: "bar",
+				OptionalSubjectsSelectors: []datastore.SubjectsSelector{
+					{
+						OptionalSubjectType: "baz",
+						RelationFilter: datastore.SubjectRelationFilter{
+							NonEllipsisRelation:      "qux",
+							OnlyNonEllipsisRelations: true,
+							IncludeEllipsisRelation:  false,
+						},
+					},
+				},
+			},
+			expectedWithoutIntegrity: "pk_relation_tuple",
+			expectedWithIntegrity:    "pk_relation_tuple",
+		},
+		{
+			name: "empty subject selector",
+			filter: datastore.RelationshipsFilter{
+				OptionalSubjectsSelectors: []datastore.SubjectsSelector{
+					{},
+				},
+			},
+			expectedWithoutIntegrity: "",
+			expectedWithIntegrity:    "",
 		},
 	}
 
-	schema := Schema(common.ColumnOptimizationOptionNone, false, false)
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			index := IndexForFilter(*schema, test.filter)
-			if test.expected == "" {
-				require.Nil(t, index)
-			} else {
-				require.NotNil(t, index)
-				require.Equal(t, test.expected, index.Name)
-			}
-		})
+	for _, withIntegrity := range []bool{false, true} {
+		integritySuffix := ""
+		if withIntegrity {
+			integritySuffix = " with integrity"
+		}
+
+		schema := Schema(common.ColumnOptimizationOptionNone, withIntegrity, false)
+		for _, test := range tests {
+			t.Run(test.name+integritySuffix, func(t *testing.T) {
+				index := IndexForFilter(*schema, test.filter)
+				expected := test.expectedWithoutIntegrity
+				if withIntegrity {
+					expected = test.expectedWithIntegrity
+				}
+
+				if expected == "" {
+					require.Nil(t, index)
+				} else {
+					require.NotNil(t, index)
+					require.Equal(t, expected, index.Name)
+				}
+			})
+		}
 	}
 }
