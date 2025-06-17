30a06cab0dc387e3836af013d282048cfb2181d5
James Thomas <james9074@gmail.com>
2025-06-17 11:42:44 -0400

    float-compare: use require.InEpsilon (or InDelta) (testifylint)

--- internal/middleware/perfinsights/perfinsights_test.go
+++ internal/middleware/perfinsights/perfinsights_test.go
@@ -96,7 +96,7 @@ func TestObserveShapeLatency(t *testing.T) {
 		require.Equal(t, io_prometheus_client.MetricType_HISTOGRAM, metric.GetType())
 
 		require.Equal(t, uint64(1), metric.GetMetric()[0].Histogram.GetSampleCount())
-		require.Equal(t, float64(0.1), metric.GetMetric()[0].Histogram.GetSampleSum())
+		require.InEpsilon(t, 0.1, metric.GetMetric()[0].Histogram.GetSampleSum(), 0.01)
 		require.Equal(t, "testMethod", metric.GetMetric()[0].Label[0].GetValue())
 
 		for _, label := range metric.GetMetric()[0].Label {
