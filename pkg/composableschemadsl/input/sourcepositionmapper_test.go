0323d0b8abd10025e589be0fc5c6c9b381ebabc0
James Thomas <james9074@gmail.com>
2025-06-17 15:45:28 -0400

    require-error: for error assertions use require (testifylint)

--- pkg/composableschemadsl/input/sourcepositionmapper_test.go
+++ pkg/composableschemadsl/input/sourcepositionmapper_test.go
@@ -5,27 +5,22 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPositionMapping(t *testing.T) {
 	mappingText, err := os.ReadFile("tests/mapping.txt")
-	if !assert.NoError(t, err, "Got error reading mapping file") {
-		return
-	}
+	require.NoError(t, err, "Got error reading mapping file")
 
 	mapper := CreateSourcePositionMapper(mappingText)
 
 	for runePosition := range mappingText {
 		lineNumber, colPosition, err := mapper.RunePositionToLineAndCol(runePosition)
-		if !assert.NoError(t, err, "Got error mapping file") {
-			return
-		}
+		require.NoError(t, err, "Got error mapping file")
 
 		// Check mapping back.
 		foundRunePosition, err := mapper.LineAndColToRunePosition(lineNumber, colPosition)
-		if !assert.NoError(t, err, "Got error mapping file") {
-			return
-		}
+		require.NoError(t, err, "Got error mapping file")
 
 		if !assert.Equal(t, runePosition, foundRunePosition, "Rune position mismatch for position %v", runePosition) {
 			return
