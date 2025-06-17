3ac91daf846a2d1c55d2327e297689f9c6e1de0b
James Thomas <james9074@gmail.com>
2025-06-17 11:22:46 -0400

    chore: ignore float-compare where we know the value is static

--- internal/datastore/revisions/hlcrevision_test.go
+++ internal/datastore/revisions/hlcrevision_test.go
@@ -108,7 +108,7 @@ func TestInexactFloat64(t *testing.T) {
 			rev, err := HLCRevisionFromString(tc)
 			require.NoError(t, err)
 
-			require.Equal(t, floatValue, rev.InexactFloat64())
+			require.Equal(t, floatValue, rev.InexactFloat64()) //nolint:testifyrequire this is based on a parsed value, so we expect it to be exactly equal.
 		})
 	}
 }
