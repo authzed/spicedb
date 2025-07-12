9f3b6dfe8b8f7a73662accdcaebcdcfb70b7af63
Mike Noseworthy <mike.noseworthy@gmail.com>
2025-07-12 15:17:41 -0230

    Parse schema from schemafile property
    Support for the `schemaFile` property in the validation file was added
    in [#2206](https://github.com/authzed/spicedb/pull/2206) but it was
    never fully integrated in to the bootstrap/import process.
    
    This change adds support similar to the validator from the `zed` cli
    which was added in [zed#478](https://github.com/authzed/zed/pull/478).
    
    If both `schema` and `schemaFile` are specified, an error is thrown. If
    only `schemaFile` is specified, the contents of the linked file are read
    and passed to the compilation function. If `schema` is specified or
    missing, this function behaves as it always has.

--- pkg/validationfile/schema_test.go
+++ pkg/validationfile/schema_test.go
@@ -4,10 +4,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	yamlv3 "gopkg.in/yaml.v3"
 
 	caveattypes "github.com/authzed/spicedb/pkg/caveats/types"
-	"github.com/authzed/spicedb/pkg/validationfile/blocks"
+	"github.com/authzed/spicedb/pkg/schemadsl/compiler"
+	"github.com/authzed/spicedb/pkg/schemadsl/input"
 )
 
 func TestParseSchema(t *testing.T) {
@@ -46,11 +46,11 @@ func TestParseSchema(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			schemaWithPosition := blocks.SchemaWithPosition{}
-			err := yamlv3.Unmarshal([]byte(tt.contents), &schemaWithPosition)
-			require.NoError(t, err)
-
-			compiled, err := CompileSchema(schemaWithPosition, caveattypes.Default.TypeSet)
+			inputSchema := compiler.InputSchema{
+				Source:       input.Source("schema"),
+				SchemaString: tt.contents,
+			}
+			compiled, err := CompileSchema(inputSchema, caveattypes.Default.TypeSet)
 			if tt.expectedError != "" {
 				require.NotNil(t, err)
 				require.Contains(t, err.Error(), tt.expectedError)
@@ -59,7 +59,6 @@ func TestParseSchema(t *testing.T) {
 				if tt.expectedDefCount > 0 {
 					require.NotNil(t, compiled)
 					require.Equal(t, tt.expectedDefCount, len(compiled.OrderedDefinitions))
-					require.Equal(t, tt.contents, schemaWithPosition.Schema)
 				}
 			}
 		})
