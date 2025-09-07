8b36218769b9f895595ee87c7d2f58bafd924dde
Meyazhagan <meyazhagan@ecyber.com>
2025-09-07 21:54:38 +0530

    refactor code : composite field

--- internal/datastore/dynamodb/composite.go
+++ internal/datastore/dynamodb/composite.go
@@ -20,56 +20,49 @@ type CompositeFields struct {
 
 type KeyValues map[string]*string
 
-func NewCompositeFields(pt string) *CompositeFields {
-
-	pattern := pt
-	rawFields := literalRe.FindAllString(pattern, -1)
-
-	fields := make([]string, len(rawFields))
-
-	for i, field := range rawFields {
-		ext := replaceRe.ReplaceAllLiteralString(field, "")
-		fields[i] = ext
-		pattern = strings.ReplaceAll(pattern,
-			fmt.Sprintf("${%s}", ext),
-			fmt.Sprintf("(?<%s>.*?)", ext),
-		)
+func NewCompositeFields(prefix string, fields ...string) *CompositeFields {
+	pattern := ""
+	if prefix != "" {
+		pattern += prefix + "#"
 	}
 
-	ptRe := regexp.MustCompile("^" + pattern + "$")
-
-	return &CompositeFields{
-		pattern: ptRe,
-		fields:  fields,
-	}
-
-}
-
-func (c CompositeFields) Build(kvp KeyValues) string {
-	result := c.pattern.String()
-
-	result = strings.TrimPrefix(result, "^")
-	result = strings.TrimSuffix(result, "$")
-
-	if len(c.fields) == 0 {
-		return ""
-	}
-
-	for _, field := range c.fields {
-		namedGroup := fmt.Sprintf("(?<%s>.*?)", field)
-
-		if value, exists := kvp[field]; exists && value != nil {
-			result = strings.ReplaceAll(result, namedGroup, *value)
-		} else {
-			return ""
+	for i, field := range fields {
+		pattern += fmt.Sprintf("(?<%s>.*?)", field)
+		if i != (len(fields) - 1) {
+			pattern += "#"
 		}
 	}
 
+	pregex := regexp.MustCompile("^" + pattern + "$")
+
+	return &CompositeFields{
+		pattern: pregex,
+		fields:  fields,
+	}
+}
+
+func NewCompositeFieldsWithoutPrefix(fields ...string) *CompositeFields {
+	return NewCompositeFields("", fields...)
+}
+
+func (c CompositeFields) Build(kvp KeyValues) string {
+	result, _ := c.BuildFull(kvp)
 	return result
 }
 
-func (c CompositeFields) BuildPartial(kvp map[string]*string) (result string, remainingFields []string) {
+func (c CompositeFields) BuildFull(kvp KeyValues) (result string, remainingFields []string) {
+	result, remainingFields, required := c.BuildPartial(kvp)
+
+	if len(required) != 0 {
+		return "", slices.Collect(maps.Keys(kvp))
+	}
+
+	return
+}
+
+func (c CompositeFields) BuildPartial(kvp map[string]*string) (result string, remainingFields []string, required []string) {
 	remainingFields = slices.Collect(maps.Keys(kvp))
+	required = slices.Clone(c.fields)
 
 	result = c.pattern.String()
 
@@ -87,11 +80,41 @@ func (c CompositeFields) BuildPartial(kvp map[string]*string) (result string, re
 			break
 		}
 
+		iu := slices.Index(required, field)
+		if iu != -1 {
+			required = append(required[:iu], required[iu+1:]...)
+		}
+
 		i := slices.Index(remainingFields, field)
-		remainingFields = append(remainingFields[:i], remainingFields[i+1:]...)
+		if i != -1 {
+			remainingFields = append(remainingFields[:i], remainingFields[i+1:]...)
+		}
 	}
 
-	return result, remainingFields
+	return
+}
+
+func (c CompositeFields) Check(availables []string) (unused []string, required []string) {
+	unused = slices.Clone(availables)
+	required = slices.Clone(c.fields)
+
+	for _, field := range c.fields {
+		if !slices.Contains(availables, field) {
+			break
+		}
+
+		i := slices.Index(required, field)
+		if i != -1 {
+			required = append(required[:i], required[i+1:]...)
+		}
+
+		iu := slices.Index(unused, field)
+		if iu != -1 {
+			unused = append(unused[:iu], unused[iu+1:]...)
+		}
+	}
+
+	return
 }
 
 func (c CompositeFields) Extract(str string, kvp KeyValues) {
--- internal/datastore/dynamodb/composite_test.go
+++ internal/datastore/dynamodb/composite_test.go
@@ -1,20 +1,20 @@
 package dynamodb
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewCompositeField(t *testing.T) {
-	comp := NewCompositeFields("REL#${namespace}#OBJ#${object_id}")
+	comp := NewCompositeFields("REL", "namespace", "object_id")
 	require.ElementsMatch(t, comp.fields, []string{"namespace", "object_id"})
-	require.Equal(t, comp.pattern.String(), "REL#(?<namespace>.*?)#OBJ#(?<object_id>.*?)")
+	require.Equal(t, comp.pattern.String(), "^REL#(?<namespace>.*?)#(?<object_id>.*?)$")
 }
 
 func TestNewCompositeFieldBuild(t *testing.T) {
-	comp := NewCompositeFields("REL#${namespace}#OBJ#${object_id}")
+	comp := NewCompositeFields("REL", "namespace", "object_id")
 	ns, oid := "user", "u1"
 	kvp := map[string]*string{
 		"namespace": &ns,
@@ -22,26 +22,79 @@ func TestNewCompositeFieldBuild(t *testing.T) {
 	}
 	res := comp.Build(kvp)
 
-	require.Equal(t, res, "REL#user#OBJ#u1")
+	require.Equal(t, res, "REL#user#u1")
 }
 
 func TestNewCompositeFieldBuildPartials(t *testing.T) {
-	comp := NewCompositeFields("REL#${namespace}#OBJ#${object_id}#${subject}")
+	comp := NewCompositeFields("REL", "namespace", "object_id", "subject")
 	ns, sub := "user", "sub"
 	kvp := map[string]*string{
 		"namespace": &ns,
 		"subject":   &sub,
 	}
-	res, remaining := comp.BuildPartial(kvp)
+	res, remaining, required := comp.BuildPartial(kvp)
 
-	fmt.Printf("%#v", remaining)
-
-	require.Equal(t, res, "REL#user#OBJ#")
+	require.Equal(t, res, "REL#user#")
 	require.ElementsMatch(t, remaining, []string{"subject"})
+	require.ElementsMatch(t, required, []string{"subject", "object_id"})
+}
+func TestNewCompositeFieldBuildFull(t *testing.T) {
+	comp := NewCompositeFields("REL", "namespace", "object_id", "subject")
+	ns, sub, oid := "user", "sub", "u1"
+	kvp := map[string]*string{
+		"namespace": &ns,
+		"subject":   &sub,
+		"object_id": &oid,
+	}
+	res, remaining := comp.BuildFull(kvp)
+
+	require.Equal(t, res, "REL#user#u1#sub")
+	require.ElementsMatch(t, remaining, []string{})
+}
+
+func TestNewCompositeFieldBuildFullRemainingFields(t *testing.T) {
+	comp := NewCompositeFields("REL", "namespace", "object_id", "subject")
+	ns, sub, oid := "user", "sub", "u1"
+	kvp := map[string]*string{
+		"namespace": &ns,
+		"subject":   &sub,
+		"object_id": &oid,
+		"relation":  aws.String("writer"),
+	}
+	res, remaining := comp.BuildFull(kvp)
+
+	require.Equal(t, res, "REL#user#u1#sub")
+	require.ElementsMatch(t, remaining, []string{"relation"})
+}
+
+func TestNewCompositeFieldBuildFullMissingOrder(t *testing.T) {
+	comp := NewCompositeFields("REL", "namespace", "object_id", "subject")
+	ns, sub := "user", "sub"
+	kvp := map[string]*string{
+		"namespace": &ns,
+		"subject":   &sub,
+		"relation":  aws.String("writer"),
+	}
+	res, remaining := comp.BuildFull(kvp)
+
+	require.Equal(t, res, "")
+	require.ElementsMatch(t, remaining, []string{"relation", "namespace", "subject"})
+}
+
+func TestNewCompositeFieldCheck(t *testing.T) {
+	comp := NewCompositeFields("REL", "namespace", "object_id", "subject")
+	availables := []string{
+		"namespace",
+		"subject",
+	}
+	unused, required := comp.Check(availables)
+
+	require.ElementsMatch(t, unused, []string{"subject"})
+	require.ElementsMatch(t, required, []string{"object_id", "subject"})
 }
 
 func TestNewCompositeFieldExtract(t *testing.T) {
-	comp := NewCompositeFields("REL#${namespace}#OBJ#${object_id}#${relation}")
+	comp := NewCompositeFields("REL", "namespace", "object_id", "relation")
 
 	var ns, rel, obj string
 
@@ -51,10 +104,9 @@ func TestNewCompositeFieldExtract(t *testing.T) {
 		"object_id": &obj,
 	}
 
-	comp.Extract("REL#user#OBJ#u1#writer", kvp)
+	comp.Extract("REL#user#u1#writer", kvp)
 
 	require.Equal(t, ns, "user")
 	require.Equal(t, obj, "u1")
 	require.Equal(t, rel, "writer")
-
 }
