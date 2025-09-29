9eb9eb9e3ca5aed292a5bd42ae9b9f9f0456cf59
Barak Michener <barak.michener@authzed.com>
2025-09-29 12:03:34 -0700

    fix: simplify the intersection arrow CheckImpl

--- pkg/query/datastore.go
+++ pkg/query/datastore.go
@@ -60,14 +60,15 @@ func (r *RelationIterator) CheckImpl(ctx *Context, resources []Object, subject O
 	// For subrelations, we need to allow type mismatches because the subrelation might bridge different types
 	// For example, group:member -> group:member should find group:everyone#member@group:engineering#member
 	// and then that relationship should be used by the Arrow to check group:engineering#member for user subjects
-	// However, wildcard relations should always enforce strict type checking
-	if subject.ObjectType != r.base.Type && r.base.Subrelation != "" && !r.base.Wildcard {
-		// For non-wildcard subrelations, we proceed with the query even if types don't match
+	// However, wildcard relations and ellipsis relations should always enforce strict type checking
+	// Ellipsis (...) means "any relation on the same type", not "bridging to a different type"
+	if subject.ObjectType != r.base.Type && r.base.Subrelation != "" && r.base.Subrelation != tuple.Ellipsis && !r.base.Wildcard {
+		// For non-wildcard, non-ellipsis subrelations, we proceed with the query even if types don't match
 		// This allows finding intermediate relationships that bridge type gaps
 		ctx.TraceStep(r, "subject type %s doesn't match base type %s, but proceeding due to subrelation %s",
 			subject.ObjectType, r.base.Type, r.base.Subrelation)
 	} else if subject.ObjectType != r.base.Type {
-		// For non-subrelations and all wildcard relations, strict type checking applies
+		// For non-subrelations, ellipsis, and all wildcard relations, strict type checking applies
 		ctx.TraceStep(r, "subject type %s doesn't match base type %s, returning empty", subject.ObjectType, r.base.Type)
 		return EmptyPathSeq(), nil
 	}
