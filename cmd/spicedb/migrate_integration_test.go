d14152bee7101ddf1a181cfa7cfb6fe0190c2432
Joseph Schorr <josephschorr@users.noreply.github.com>
2025-10-31 13:13:31 -0400

    chore: reduce chance of flakiness on the migrate integration test
    Adds retries and increases the wait timeout for the migration call

--- cmd/spicedb/migrate_integration_test.go
+++ cmd/spicedb/migrate_integration_test.go
@@ -43,57 +43,71 @@ func TestMigrate(t *testing.T) {
 		}
 
 		t.Run(engineKey, func(t *testing.T) {
-			engineKey := engineKey
+			for retryCount := range 5 {
+				engineKey := engineKey
 
-			r := testdatastore.RunDatastoreEngineWithBridge(t, engineKey, bridgeNetworkName)
-			db := r.NewDatabase(t)
+				r := testdatastore.RunDatastoreEngineWithBridge(t, engineKey, bridgeNetworkName)
+				db := r.NewDatabase(t)
 
-			envVars := []string{}
-			if wev, ok := r.(testdatastore.RunningEngineForTestWithEnvVars); ok {
-				envVars = wev.ExternalEnvVars()
-			}
-
-			// Run the migrate command and wait for it to complete.
-			resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-				Repository: "authzed/spicedb",
-				Tag:        "ci",
-				Cmd:        []string{"migrate", "head", "--datastore-engine", engineKey, "--datastore-conn-uri", db},
-				NetworkID:  bridgeNetworkName,
-				Env:        envVars,
-			}, func(config *docker.HostConfig) {
-				config.RestartPolicy = docker.RestartPolicy{
-					Name: "no",
+				envVars := []string{}
+				if wev, ok := r.(testdatastore.RunningEngineForTestWithEnvVars); ok {
+					envVars = wev.ExternalEnvVars()
 				}
-			})
-			require.NoError(t, err)
 
-			waitCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-			defer cancel()
-
-			// Ensure the command completed successfully.
-			status, err := pool.Client.WaitContainerWithContext(resource.Container.ID, waitCtx)
-			require.NoError(t, err)
-
-			if status != 0 {
-				stream := new(bytes.Buffer)
-
-				lerr := pool.Client.Logs(docker.LogsOptions{
-					Context:      waitCtx,
-					OutputStream: stream,
-					ErrorStream:  stream,
-					Stdout:       true,
-					Stderr:       true,
-					Container:    resource.Container.ID,
+				// Run the migrate command and wait for it to complete.
+				resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+					Repository: "authzed/spicedb",
+					Tag:        "ci",
+					Cmd:        []string{"migrate", "head", "--datastore-engine", engineKey, "--datastore-conn-uri", db},
+					NetworkID:  bridgeNetworkName,
+					Env:        envVars,
+				}, func(config *docker.HostConfig) {
+					config.RestartPolicy = docker.RestartPolicy{
+						Name: "no",
+					}
 				})
-				require.NoError(t, lerr)
+				if err != nil && retryCount < 4 {
+					// On error, retry a few times before failing
+					t.Logf("retrying migration for engine %s due to error: %v", engineKey, err)
+					continue
+				}
 
-				require.Fail(t, "Got non-zero exit code", stream.String())
+				require.NoError(t, err)
+
+				waitCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				defer cancel()
+
+				// Ensure the command completed successfully.
+				status, err := pool.Client.WaitContainerWithContext(resource.Container.ID, waitCtx)
+				if err != nil && retryCount < 4 {
+					// On error, retry a few times before failing
+					t.Logf("retrying migration for engine %s due to error: %v", engineKey, err)
+					continue
+				}
+
+				require.NoError(t, err)
+
+				if status != 0 {
+					stream := new(bytes.Buffer)
+
+					lerr := pool.Client.Logs(docker.LogsOptions{
+						Context:      waitCtx,
+						OutputStream: stream,
+						ErrorStream:  stream,
+						Stdout:       true,
+						Stderr:       true,
+						Container:    resource.Container.ID,
+					})
+					require.NoError(t, lerr)
+
+					require.Fail(t, "Got non-zero exit code", stream.String())
+				}
+
+				t.Cleanup(func() {
+					// When you're done, kill and remove the container
+					_ = pool.Purge(resource)
+				})
 			}
-
-			t.Cleanup(func() {
-				// When you're done, kill and remove the container
-				_ = pool.Purge(resource)
-			})
 		})
 	}
 }
