0613db3938ece5f967407fe47b98a239741eb3de
Víctor Roldán Betancort <vroldanbet@authzed.com>
2025-11-24 15:46:35 +0000

    testing if there is a regression in watch api

--- internal/datastore/postgres/revisions.go
+++ internal/datastore/postgres/revisions.go
@@ -404,11 +404,9 @@ func (pr postgresRevision) MarshalBinary() ([]byte, error) {
 		return nil, spiceerrors.MustBugf("could not safely cast snapshot xmax to int64: %v", err)
 	}
 	protoRevision := implv1.PostgresRevision{
-		Xmin:              pr.snapshot.xmin,
-		RelativeXmax:      relativeXmax - xminInt,
-		RelativeXips:      relativeXips,
-		OptionalTxid:      pr.optionalTxID.Uint64,
-		OptionalTimestamp: pr.optionalNanosTimestamp,
+		Xmin:         pr.snapshot.xmin,
+		RelativeXmax: relativeXmax - xminInt,
+		RelativeXips: relativeXips,
 	}
 
 	return protoRevision.MarshalVT()
