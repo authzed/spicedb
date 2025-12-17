616e0c763dee6e44df14e939b55a2081dee37a1c
ivanauth <ivan@authzed.com>
2025-12-17 12:20:41 -0500

    fix: re-enable async logging with proper shutdown flushing

--- pkg/testutil/leakdetection.go
+++ pkg/testutil/leakdetection.go
@@ -9,5 +9,7 @@ func GoLeakIgnores() []goleak.Option {
 		// TODO: https://github.com/googleapis/google-cloud-go/issues/14228
 		// when that is complete, remove this line
 		goleak.IgnoreAnyFunction("go.opencensus.io/stats/view.(*worker).start"),
+		// Async logging diode writer has a polling goroutine that flushes buffered logs
+		goleak.IgnoreAnyFunction("github.com/rs/zerolog/diode.Writer.poll"),
 	}
 }
