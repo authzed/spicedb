1096f00d8bbefc04a7c74523db87b63ddeee6991
ivanauth <ivan@authzed.com>
2026-01-08 10:50:01 -0500

    feat: allow underscore prefix for private identifiers

--- e2e/newenemy/newenemy_test.go
+++ e2e/newenemy/newenemy_test.go
@@ -64,7 +64,7 @@ definition {{.Resource}} {
 {{ end }}
 `
 	objIDRegex           = "[a-zA-Z0-9_][a-zA-Z0-9/_-]{0,127}"
-	namespacePrefixRegex = "[a-z_][a-z0-9_]{1,62}[a-z0-9]"
+	namespacePrefixRegex = "[a-z][a-z0-9_]{1,62}[a-z0-9]"
 )
 
 var (
