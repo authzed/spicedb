b242e44fd3265ee4f0945747da1a66541db99b91
Tanner Stirrat <tstirrat@gmail.com>
2026-01-28 07:26:23 -0700

    fix: make schemautil.ApplySchemaChangesOverExisting preserve existing stuff (#2852)
    Co-authored-by: Maria Ines Parnisari <maria.ines.parnisari@authzed.com>

--- internal/services/shared/schema.go
+++ internal/services/shared/schema.go
@@ -26,8 +26,10 @@ type ValidatedSchemaChanges struct {
 	validatedDefinitions map[string]*schema.ValidatedDefinition
 	newCaveatDefNames    *mapz.Set[string]
 	newObjectDefNames    *mapz.Set[string]
-	additiveOnly         bool
-	schemaText           string
+	// additiveOnly indicates whether all operations should be applied
+	// or only those operations which are additive (i.e. not changes or deletions)
+	additiveOnly bool
+	schemaText   string
 }
 
 // ValidateSchemaChanges validates the schema found in the compiled schema and returns a
@@ -119,6 +121,8 @@ func ApplySchemaChanges(ctx context.Context, rwt datastore.ReadWriteTransaction,
 
 // ApplySchemaChangesOverExisting applies schema changes found in the validated changes struct, against
 // existing caveat and object definitions given.
+// The idea is that the given schema will be diffed against the existing objects given, and any
+// objects not named in one of those two which are present in the datastore will be preserved.
 func ApplySchemaChangesOverExisting(
 	ctx context.Context,
 	rwt datastore.ReadWriteTransaction,
@@ -227,14 +231,43 @@ func ApplySchemaChangesOverExisting(
 			return nil, err
 		}
 
+		// Get the list of extant definitions so that we can add them to the
+		// list of definitions that should be written in the single shot
+		reader, err := rwt.SchemaReader()
+		if err != nil {
+			return nil, err
+		}
+		allExtantDefinitions, err := reader.ListAllSchemaDefinitions(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		writtenDefinitionNames := mapz.NewSet[string]()
+		for _, def := range validated.compiled.CaveatDefinitions {
+			writtenDefinitionNames.Add(def.Name)
+		}
+		for _, def := range validated.compiled.ObjectDefinitions {
+			writtenDefinitionNames.Add(def.Name)
+		}
+
+		changedDefinitionNames := writtenDefinitionNames.Union(removedObjectDefNames).Union(removedCaveatDefNames)
+
+		unchangedDefinitions := make([]datastore.SchemaDefinition, 0)
+		for _, def := range allExtantDefinitions {
+			if !changedDefinitionNames.Has(def.GetName()) {
+				unchangedDefinitions = append(unchangedDefinitions, def)
+			}
+		}
+
 		// Build the full list of schema definitions to write
-		definitions := make([]datastore.SchemaDefinition, 0, len(validated.compiled.ObjectDefinitions)+len(validated.compiled.CaveatDefinitions))
+		definitions := make([]datastore.SchemaDefinition, 0, len(validated.compiled.ObjectDefinitions)+len(validated.compiled.CaveatDefinitions)+len(unchangedDefinitions))
 		for _, caveatDef := range validated.compiled.CaveatDefinitions {
 			definitions = append(definitions, caveatDef)
 		}
 		for _, objectDef := range validated.compiled.ObjectDefinitions {
 			definitions = append(definitions, objectDef)
 		}
+		definitions = append(definitions, unchangedDefinitions...)
 
 		// WriteSchema will handle writing new/changed definitions and deleting removed ones
 		if err := schemaWriter.WriteSchema(ctx, definitions, validated.schemaText, caveatTypeSet); err != nil {
--- internal/services/shared/schema_test.go
+++ internal/services/shared/schema_test.go
@@ -632,3 +632,156 @@ definition resource {
 		})
 	}
 }
+
+func TestApplySchemaChangesOverExisting(t *testing.T) {
+	tcs := []struct {
+		name                         string
+		staticSchema                 string
+		startingSchema               string
+		patchSchema                  string
+		expectedSchema               string
+		expectedAppliedSchemaChanges AppliedSchemaChanges
+		expectedError                string
+	}{
+		{
+			name:         "empty static schema",
+			staticSchema: "",
+			startingSchema: `
+				definition user {}
+
+				definition document {
+					relation viewer: user
+					permission view = viewer
+				}
+
+				caveat hasFortyTwo(value int) {
+				value == 42
+				}
+			`,
+			patchSchema: `
+				definition user {}
+
+				definition organization {
+					relation member: user
+					permission admin = member
+				}
+
+				caveat catchTwentyTwo(value int) {
+				value == 22
+				}
+			`,
+			expectedSchema: "caveat catchTwentyTwo(value int) {\n\tvalue == 22\n}\n\ndefinition organization {\n\trelation member: user\n\tpermission admin = member\n}\n\ndefinition user {}",
+			expectedAppliedSchemaChanges: AppliedSchemaChanges{
+				// NOTE: this is 5 because the `user` definition is written even though it's unchanged
+				TotalOperationCount:   5,
+				NewObjectDefNames:     []string{"organization"},
+				RemovedObjectDefNames: []string{"document"},
+				NewCaveatDefNames:     []string{"catchTwentyTwo"},
+				RemovedCaveatDefNames: []string{"hasFortyTwo"},
+			},
+		},
+		{
+			name: "basic static schema",
+			staticSchema: `
+			definition admin {}
+			`,
+			startingSchema: `
+				definition user {}
+
+				definition document {
+					relation viewer: user
+					permission view = viewer
+				}
+
+				caveat hasFortyTwo(value int) {
+				value == 42
+				}
+			`,
+			patchSchema: `
+				definition user {}
+
+				definition organization {
+					relation member: user
+					permission admin = member
+				}
+
+				caveat catchTwentyTwo(value int) {
+				value == 22
+				}
+			`,
+			expectedSchema: "caveat catchTwentyTwo(value int) {\n\tvalue == 22\n}\n\ndefinition admin {}\n\ndefinition organization {\n\trelation member: user\n\tpermission admin = member\n}\n\ndefinition user {}",
+			// NOTE: we're expecting that the `admin` part of the schema stays there.
+			expectedAppliedSchemaChanges: AppliedSchemaChanges{
+				// NOTE: this is 5 because the `user` definition is written even though it's unchanged
+				TotalOperationCount:   5,
+				NewObjectDefNames:     []string{"organization"},
+				RemovedObjectDefNames: []string{"document"},
+				NewCaveatDefNames:     []string{"catchTwentyTwo"},
+				RemovedCaveatDefNames: []string{"hasFortyTwo"},
+			},
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			require := require.New(t)
+			rawDS, err := dsfortesting.NewMemDBDatastoreForTesting(t, 0, 0, memdb.DisableGC)
+			require.NoError(err)
+
+			// NOTE: the schema that we start with in the DB is the concatenation of the
+			// static part of the schema and the written part of the schema because
+			// the function under tests takes the "starting" schema (i.e. the part of the schema
+			// that's expected to be modified by the written schema) as one of its arguments.
+			// It ignores what's already in the database that isn't explicitly named.
+			schemaInDB := tc.staticSchema + "\n\n" + tc.startingSchema
+
+			compiledStartingSchema, err := compiler.Compile(compiler.InputSchema{
+				Source:       input.Source("schema"),
+				SchemaString: tc.startingSchema,
+			}, compiler.AllowUnprefixedObjectType())
+			require.NoError(err)
+
+			ds, _ := testfixtures.DatastoreFromSchemaAndTestRelationships(rawDS, schemaInDB, nil, require)
+
+			// Update the schema and ensure it works.
+			compiled, err := compiler.Compile(compiler.InputSchema{
+				Source:       input.Source("schema"),
+				SchemaString: tc.patchSchema,
+			}, compiler.AllowUnprefixedObjectType())
+			require.NoError(err)
+
+			validated, err := ValidateSchemaChanges(t.Context(), compiled, caveattypes.Default.TypeSet, false, tc.patchSchema)
+			if tc.expectedError != "" {
+				require.ErrorContains(err, tc.expectedError)
+				return
+			}
+
+			require.NoError(err)
+
+			_, err = ds.ReadWriteTx(t.Context(), func(ctx context.Context, rwt datastore.ReadWriteTransaction) error {
+				applied, err := ApplySchemaChangesOverExisting(
+					t.Context(),
+					rwt,
+					caveattypes.Default.TypeSet,
+					validated,
+					compiledStartingSchema.CaveatDefinitions,
+					compiledStartingSchema.ObjectDefinitions,
+				)
+				if tc.expectedError != "" {
+					require.EqualError(err, tc.expectedError)
+					return nil
+				}
+
+				require.NoError(err)
+				require.Equal(tc.expectedAppliedSchemaChanges, *applied)
+
+				reader, err := rwt.SchemaReader()
+				require.NoError(err)
+				schema, err := reader.SchemaText()
+				require.NoError(err)
+				require.Equal(tc.expectedSchema, schema)
+				return nil
+			})
+			require.NoError(err)
+		})
+	}
+}
