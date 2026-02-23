e404e01ecd85fa9263d68438d3f187cd5421d499
Stefano Scafiti <stefano.scafiti96@gmail.com>
2026-02-23 16:09:52 +0100

    ts nullable

--- internal/datastore/postgres/revisions.go
+++ internal/datastore/postgres/revisions.go
@@ -110,7 +110,7 @@ func (pgd *pgDatastore) optimizedRevisionFunc(ctx context.Context) (datastore.Re
 	var revision xid8
 	var snapshot pgSnapshot
 	var validForNanos time.Duration
-	var timestamp time.Time
+	var timestamp *time.Time
 	var err error
 
 	if err := pgd.readPool.QueryRow(ctx, pgd.optimizedRevisionQuery).
@@ -118,9 +118,12 @@ func (pgd *pgDatastore) optimizedRevisionFunc(ctx context.Context) (datastore.Re
 		return datastore.NoRevision, 0, fmt.Errorf(errRevision, err)
 	}
 
-	tsNanos, err := safecast.Convert[uint64](timestamp.UnixNano())
-	if err != nil {
-		return nil, 0, spiceerrors.MustBugf("could not cast timestamp to uint64")
+	var tsNanos uint64
+	if timestamp != nil {
+		tsNanos, err = safecast.Convert[uint64](timestamp.UnixNano())
+		if err != nil {
+			return nil, 0, spiceerrors.MustBugf("could not cast timestamp to uint64")
+		}
 	}
 
 	snapshot = snapshot.markComplete(revision.Uint64)
