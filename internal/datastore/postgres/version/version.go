d24da5ed69a319e5f60ab394173aff7c9bd0ec44
Tanner Stirrat <tstirrat@gmail.com>
2026-03-02 15:40:25 -0700

    chore: update versions in datastore support tags

--- internal/datastore/postgres/version/version.go
+++ internal/datastore/postgres/version/version.go
@@ -3,9 +3,9 @@ package version
 // MinimumSupportedPostgresVersion is the minimum version of Postgres supported for this driver.
 //
 // NOTE: must match a tag on DockerHub for the `postgres` image.
-const MinimumSupportedPostgresVersion = "13.8"
+const MinimumSupportedPostgresVersion = "14"
 
 // LatestTestedPostgresVersion is the latest version of Postgres that has been tested with this driver.
 //
 // NOTE: must match a tag on DockerHub for the `postgres` image.
-const LatestTestedPostgresVersion = "17.2"
+const LatestTestedPostgresVersion = "18"
