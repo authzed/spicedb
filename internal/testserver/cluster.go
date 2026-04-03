6560e777604b67868f20c7b5d6571568e8849561
Maria Ines Parnisari <maria.ines.parnisari@authzed.com>
2026-04-02 17:13:52 -0700

    ci: minor improvements to BenchmarkServices (#2869)

--- internal/testserver/cluster.go
+++ internal/testserver/cluster.go
@@ -137,14 +137,10 @@ func (r *SafeManualResolver) ResolveNow(_ resolver.ResolveNowOptions) {
 // Close implements the resolver.Resolver interface
 func (r *SafeManualResolver) Close() {}
 
-// TestClusterWithDispatch creates a cluster with `size` nodes
-// The cluster has a real dispatch stack that uses bufconn grpc connections
+// TestClusterWithDispatch creates a cluster with `size` nodes.
+// The cluster has a real dispatch stack that uses bufconn grpc connections.
+// All Caching is turned off.
 func TestClusterWithDispatch(t testing.TB, size uint, ds datastore.Datastore, additionalServerOptions ...server.ConfigOption) ([]*grpc.ClientConn, func()) {
-	return TestClusterWithDispatchAndCacheConfig(t, size, ds, additionalServerOptions...)
-}
-
-// TestClusterWithDispatchAndCacheConfig creates a cluster with `size` nodes and with cache toggled.
-func TestClusterWithDispatchAndCacheConfig(t testing.TB, size uint, ds datastore.Datastore, additionalServerOptions ...server.ConfigOption) ([]*grpc.ClientConn, func()) {
 	// each cluster gets a unique prefix since grpc resolution is process-global
 	prefix := getPrefix(t)
 
@@ -220,7 +216,9 @@ func TestClusterWithDispatchAndCacheConfig(t testing.TB, size uint, ds datastore
 		serverOptions = append(serverOptions, additionalServerOptions...)
 
 		ctx, cancel := context.WithCancel(t.Context())
-		srv, err := server.NewConfigWithOptionsAndDefaults(serverOptions...).Complete(ctx)
+		cfg := server.NewConfigWithOptionsAndDefaults(serverOptions...)
+		t.Log(cfg.FlatDebugMap())
+		srv, err := cfg.Complete(ctx)
 		require.NoError(t, err)
 
 		errCh := make(chan error, 1)
